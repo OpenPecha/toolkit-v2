fce29b2ba93df059c1bd9f473949695c0b6882c2
Tenzin <tenzintsunduebhattu@gmail.com>
2024-07-01 16:34:58 +0530

    create/class Annotation

--- src/openpecha/pecha/__init__.py
+++ src/openpecha/pecha/__init__.py
@@ -1,11 +1,14 @@
 from typing import Dict
 
+from openpecha.pecha.annotation import Annotation
+
 
 class Pecha:
     def __init__(self, pecha_id: str, segments: Dict[str, str]) -> None:
         self.pecha_id = pecha_id
         self.segments = segments
         self.base_text = "".join(segments.values())
+        self.annotations = self.build_annotations()
 
     @classmethod
     def from_path(cls, path: str):
@@ -14,3 +17,15 @@ class Pecha:
     @classmethod
     def from_id(cls, pecha_id: str):
         pass
+
+    def build_annotations(self):
+        char_count = 0
+        for segment_id, segment in self.segments.items():
+            annotation = Annotation(
+                annotation_id=segment_id,
+                segment=segment,
+                start=char_count,
+                end=char_count + len(segment),
+            )
+            char_count += len(segment)
+            yield annotation
