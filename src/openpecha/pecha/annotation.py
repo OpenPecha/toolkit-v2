6fd99d19a2dd3b3b209f8fea1e4dd8219ea762c4
Tenzin <tenzintsunduebhattu@gmail.com>
2024-07-03 15:01:22 +0530

    join class Annotation in Layer field

--- src/openpecha/pecha/annotation.py
+++ src/openpecha/pecha/annotation.py
@@ -1,8 +1,10 @@
 from pydantic import BaseModel, Field, ValidationInfo, field_validator
 
+from openpecha.ids import get_uuid
+
 
 class Annotation(BaseModel):
-    id_: str
+    id_: str = Field(default_factory=get_uuid)
     segment: str
     start: int = Field(ge=0)
     end: int = Field(ge=0)
--- src/openpecha/pecha/layer.py
+++ src/openpecha/pecha/layer.py
@@ -1,8 +1,10 @@
 from enum import Enum
+from typing import Dict
 
 from pydantic import BaseModel, Field
 
 from openpecha.ids import get_uuid
+from openpecha.pecha.annotation import Annotation
 
 
 class LayerEnum(Enum):
@@ -11,6 +13,6 @@ class LayerEnum(Enum):
 
 
 class Layer(BaseModel):
-    id: str = Field(default_factory=get_uuid)
+    id_: str = Field(default_factory=get_uuid)
     annotation_type: LayerEnum
-    annotations: dict = Field(default_factory=dict)
+    annotations: Dict[str, Annotation] = Field(default_factory=dict)
