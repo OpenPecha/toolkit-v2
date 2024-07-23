42cc3faedae298b87f8a989184ef53c3bee2810e
tenzin3 <tenzintsunduebhattu@gmail.com>
2024-07-23 16:58:55 +0530

    AlingmentMetaData

--- src/openpecha/alignment/metadata.py
+++ src/openpecha/alignment/metadata.py
@@ -1,7 +1,9 @@
 from enum import Enum
 from typing import Dict
 
-from pydantic import BaseModel
+from pydantic import BaseModel, model_validator
+
+from openpecha.ids import get_alignment_id
 
 
 class AlignmentRelationEnum(Enum):
@@ -29,4 +31,27 @@ class SegmentMetaData(BaseModel):
 
 
 class AlignmentMetaData(BaseModel):
+    id_: str
     segments_metadata: Dict[str, SegmentMetaData]
+
+    @classmethod
+    def from_dict(cls, metadata: Dict) -> "AlignmentMetaData":
+        segments_metadata: Dict[str, SegmentMetaData] = {}
+        for segment_source_id, segment_metadata in metadata.items():
+            type = AlignmentTypeEnum(segment_metadata["type"])
+            relation = AlignmentRelationEnum(segment_metadata["relation"])
+            lang = LanguageEnum(segment_metadata["lang"])
+            base = segment_metadata["base"]
+
+            segments_metadata[segment_source_id] = SegmentMetaData(
+                type=type, relation=relation, lang=lang, base=base
+            )
+
+        return cls(segments_metadata=segments_metadata)
+
+    @model_validator(mode="before")
+    @classmethod
+    def set_id(cls, values):
+        if "id_" not in values or values["id_"] is None:
+            values["id_"] = get_alignment_id()
+        return values
