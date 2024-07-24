12d7ee3be6694e16092ce7a954562bef2332c218
tenzin3 <tenzintsunduebhattu@gmail.com>
2024-07-24 11:23:45 +0530

    assign uuid for alingment anns

--- src/openpecha/alignment/__init__.py
+++ src/openpecha/alignment/__init__.py
@@ -1,14 +1,16 @@
-from typing import List, Tuple
+from typing import Dict, List, Tuple
 
 from openpecha.alignment.metadata import AlignmentMetaData
+from openpecha.ids import get_uuid
 
 
 class Alignment:
     def __init__(
         self,
         metadata: AlignmentMetaData,
-        segment_pairs=None,
+        segment_pairs: Dict[str, Dict[str, str]] = None,
     ):
+        self.id = metadata.id_
         self.metadata = metadata
         self.segment_pairs = segment_pairs
 
@@ -26,7 +28,14 @@ class Alignment:
         segment_pairs: List[Tuple[Tuple[str, str], Tuple[str, str]]],
         metadata: AlignmentMetaData,
     ):
-        return cls(metadata=metadata, segment_pairs=segment_pairs)
+        """assign uuid for each alignment id"""
+        transformed_segment_pairs = {}
+        for (source_id, source_ann_id), (target_id, target_ann_id) in segment_pairs:
+            transformed_segment_pairs[get_uuid()] = {
+                source_id: source_ann_id,
+                target_id: target_ann_id,
+            }
+        return cls(metadata=metadata, segment_pairs=transformed_segment_pairs)
 
     def save(self, path: str):
         pass
