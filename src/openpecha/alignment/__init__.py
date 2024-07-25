0b0a72928c9aae7230051bce823be76a5d473c3e
tenzin3 <tenzintsunduebhattu@gmail.com>
2024-07-25 23:43:43 +0530

    fix load_pechas

--- src/openpecha/alignment/__init__.py
+++ src/openpecha/alignment/__init__.py
@@ -27,7 +27,7 @@ class Alignment:
         )
         self.metadata = metadata
         self.segment_pairs = segment_pairs
-        self.pechas = pechas if pechas else self.load_pechas()
+        self.pechas = pechas
 
     @classmethod
     def from_path(cls, path: Path, pechas: Dict[str, Pecha] = None):
@@ -74,7 +74,9 @@ class Alignment:
         pechas: Dict[str, Pecha] = {
             pecha_id: Pecha.from_id(pecha_id) for pecha_id in pecha_ids
         }
-        return pechas
+        if self.pechas is None:
+            self.pechas = pechas
+        return self.pechas
 
     def write(self, output_path: Path):
         """ """
