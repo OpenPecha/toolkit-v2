8aad08cf0241dc9292f0571aba6719bc53041691
tenzin3 <tenzintsunduebhattu@gmail.com>
2024-08-02 10:34:52 +0530

    test for pecha_db alignment serializer

--- src/openpecha/alignment/serializers/pecha_db.py
+++ src/openpecha/alignment/serializers/pecha_db.py
@@ -1,4 +1,5 @@
 import json
+import re
 from collections import defaultdict
 from pathlib import Path
 from typing import Dict, List
@@ -13,7 +14,7 @@ class PechaDbSerializer:
         self.segment_pairs = segment_pairs
         self.alignment_metadata = alignment_metadata
 
-    def serialize(self, output_path: Path = Path(".")):
+    def serialize(self, output_path: Path = Path(".")) -> Path:
         pecha_db_json: Dict = defaultdict(lambda: defaultdict(dict))
         pecha_lang = {}
         for pecha_id, pecha in self.alignment_metadata.segments_metadata.items():
@@ -21,7 +22,7 @@ class PechaDbSerializer:
                 pecha_db_json["target"]["books"] = {
                     "title": "pecha title",  # Work need here
                     "language": "bo",
-                    "version_source": f"github.com/PechaData/{pecha}/base/{pecha.base}.txt",
+                    "version_source": f"www.github.com/PechaData/{pecha_id}/base/{pecha.base}.txt",
                     "direction": "ltr",
                     "content": [],
                 }
@@ -29,21 +30,41 @@ class PechaDbSerializer:
                 pecha_db_json["source"]["books"] = {
                     "title": "pecha title",
                     "language": "en",
-                    "version_source": f"github.com/PechaData/{pecha}/base/{pecha.base}.txt",
+                    "version_source": f"wwww.github.com/PechaData/{pecha_id}/base/{pecha.base}.txt",
                     "direction": "ltr",
                     "content": [],
                 }
             pecha_lang[pecha_id] = pecha.lang
 
+        curr_chapter: Dict = {}
+        for pecha_id in pecha_lang.keys():
+            curr_chapter[pecha_id] = []
+
         for segment_pair in self.segment_pairs:
             segment_pair_data = next(iter(segment_pair.values()))
             for pecha_id, segment in segment_pair_data.items():
-                if pecha_lang[pecha_id] == LanguageEnum.tibetan:
-                    pecha_db_json["target"]["books"]["content"].append(segment)
-                elif pecha_lang[pecha_id] == LanguageEnum.english:
-                    pecha_db_json["source"]["books"]["content"].append(segment)
+                if re.match(r"^Ch \d+ ", segment):  # Start of a new chapter
+                    if pecha_lang[pecha_id] == LanguageEnum.tibetan:
+                        pecha_db_json["target"]["books"]["content"].append(
+                            curr_chapter[pecha_id]
+                        )
+                    elif pecha_lang[pecha_id] == LanguageEnum.english:
+                        pecha_db_json["source"]["books"]["content"].append(
+                            curr_chapter[pecha_id]
+                        )
+                    curr_chapter[pecha_id] = []
+                    continue
+                curr_chapter[pecha_id].append(segment)
 
-        with open(
-            output_path / f"{self.alignment_metadata.id_}.json", "w", encoding="utf-8"
-        ) as f:
+        """ Adding the rest of the segments """
+        for pecha_id, segment in curr_chapter.items():
+            if pecha_lang[pecha_id] == LanguageEnum.tibetan:
+                pecha_db_json["target"]["books"]["content"].append(segment)
+            elif pecha_lang[pecha_id] == LanguageEnum.english:
+                pecha_db_json["source"]["books"]["content"].append(segment)
+
+        output_file = output_path / f"{self.alignment_metadata.id_}.json"
+        with open(output_file, "w", encoding="utf-8") as f:
             json.dump(pecha_db_json, f, ensure_ascii=False, indent=2)
+
+        return output_file
