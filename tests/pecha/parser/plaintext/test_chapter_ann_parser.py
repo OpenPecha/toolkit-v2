5f927ff6787ea11d32be11c31ec9c5a7611a5fff
tenzin3 <tenzintsunduebhattu@gmail.com>
2024-08-05 14:47:17 +0530

    more test cases

--- tests/pecha/parser/plaintext/test_chapter_ann_parser.py
+++ tests/pecha/parser/plaintext/test_chapter_ann_parser.py
@@ -17,6 +17,15 @@ def test_plain_text_chapter_ann_parser():
     assert isinstance(chapter_ann_parser, PlainTextChapterAnnotationParser)
     pecha_path = chapter_ann_parser.parse(DATA)
 
+    assert pecha_path.exists()
+    assert (pecha_path / "metadata.json").exists()
+    assert (pecha_path / "base").exists()
+    assert (pecha_path / "layers").exists()
+    """ check there is only one json file in layers """
+    assert len(list((pecha_path / "layers").glob("*.json"))) == 1
+    """ check there is one txt file in base """
+    assert len(list((pecha_path / "base").glob("*.txt"))) == 1
+
     """ clean up """
     rmtree(pecha_path)
 
