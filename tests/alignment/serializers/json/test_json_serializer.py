8efeb8646e97dffef9f6f3a680a5a0d7a538a44c
tenzin3 <tenzintsunduebhattu@gmail.com>
2024-08-24 19:37:38 +0530

    update data for alignment json serializer

--- tests/alignment/serializers/json/test_json_serializer.py
+++ tests/alignment/serializers/json/test_json_serializer.py
@@ -10,13 +10,13 @@ from openpecha.pecha import Pecha
 def test_json_serializer():
     DATA = Path(__file__).parent / "data"
 
-    alignment_path = DATA / "A84686EBF"
+    alignment_path = DATA / "A26734E88"
 
     alignment = Alignment.from_path(alignment_path)
     serializer = JSONSerializer(alignment)
 
-    source_pecha = Pecha.from_path(DATA / "I79F4F48B")
-    target_pecha = Pecha.from_path(DATA / "IF1FF2A77")
+    source_pecha = Pecha.from_path(DATA / "IB6D0A20F")
+    target_pecha = Pecha.from_path(DATA / "IFBDF1D6F")
 
     source_ann_store, target_ann_store = serializer.load_pechas(
         source_pecha, target_pecha
