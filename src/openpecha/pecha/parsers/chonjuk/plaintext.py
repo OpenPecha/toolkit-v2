ce6ad59d31654369c29ba386c0dbb3606982dc5a
tenzin3 <tenzintsunduebhattu@gmail.com>
2024-10-04 10:58:41 +0530

    fix/Chonjuk chapter parser

--- src/openpecha/pecha/parsers/chonjuk/plaintext.py
+++ src/openpecha/pecha/parsers/chonjuk/plaintext.py
@@ -2,29 +2,33 @@ import re
 from pathlib import Path
 
 from openpecha.config import PECHAS_PATH
-from openpecha.pecha.parsers import BaseParser, Document
+from openpecha.pecha import Pecha
+from openpecha.pecha.parsers import BaseParser
 
 
 class ChonjukPlainTextParser(BaseParser):
     def __init__(self, text: str):
-        super().__init__(text)
-        self.components = [ChapterParser()]
+        self.text = text
+        self.components = [ChapterParser(self.text)]
 
     def parse(self, output_path: Path = PECHAS_PATH):
-        doc = Document(text=self.text)
-        for component in self.components:
-            doc = component(doc)
+        pecha = Pecha.create(output_path)
+        base_name = pecha.set_base(self.text)
 
-        return doc
+        for component in self.components:
+            component(base_name)
 
 
 class ChapterParser:
-    def __call__(self, doc: Document):
+    def __init__(self, text: str):
+        self.text = text
+
+    def __call__(self, base_name: str):
         chapter_regex = (
             r"ch(\d+)-\"([\u0F00-\u0FFF]+)\"\s*([\u0F00-\u0FFF\s\n]+)[\u0F00-\u0FFF]"
         )
         # Find all matches
-        matches = re.finditer(chapter_regex, doc.text)
+        matches = re.finditer(chapter_regex, self.text)
 
         chapter_matches = []
         # Iterate over the matches and store the spans
@@ -40,21 +44,22 @@ class ChapterParser:
         def keep_groups(match):
             return " ".join(match.groups())
 
-        doc.resources["clean_text"] = re.sub(chapter_regex, keep_groups, doc.text)
+        cleaned_text = re.sub(chapter_regex, keep_groups, self.text)  # noqa
         # Get the updated chapter co-ordinate
         updated_chapter_matches = []
         offset = 0
         for chapter_match in chapter_matches:
+            offset += 2  # Account for 'ch' and '-'
             chapter_no_span = chapter_match["chapter_number_span"]
             updated_chapter_no_span = (
-                chapter_no_span[0] - offset - 2,
-                chapter_no_span[1] - offset - 2,
+                chapter_no_span[0] - offset,
+                chapter_no_span[1] - offset,
             )
-            offset += 3  # Account for 'ch', '-' , '"' and substract a space
+            offset += 1  # Account for '"'
 
             title_span = chapter_match["title_span"]
             updated_title_span = (title_span[0] - offset, title_span[1] - offset)
-            offset += 0  # Account for  '"' and substract a space
+            offset += 1  # Account for  '"' and substract a space
 
             content_span = chapter_match["content_span"]
             spaces = content_span[0] - title_span[1] - 1
@@ -68,7 +73,3 @@ class ChapterParser:
                     "content_span": updated_content_span,
                 }
             )
-
-        doc.annotations["chapter"] = updated_chapter_matches
-
-        return doc
--- tests/pecha/parser/chonjuk/test_plaintext.py
+++ tests/pecha/parser/chonjuk/test_plaintext.py
@@ -8,6 +8,7 @@ class TestChonjukPlainTextParser:
         data = Path(__file__).parent / "data"
         chonjuk_text = (data / "chonjuk.txt").read_text(encoding="utf-8")
         parser = ChonjukPlainTextParser(chonjuk_text)
-        doc = parser.parse()
+        parser.parse()
 
-        assert doc.text == chonjuk_text
+
+TestChonjukPlainTextParser().test_chonjuk_plaintext_parser()
