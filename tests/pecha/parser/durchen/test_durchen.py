8870906a6211056306b7a8795fde9c289cd9bf44
tenzin3 <tenzintsunduebhattu@gmail.com>
2024-10-16 12:34:39 +0530

    improve test for durchen

--- tests/pecha/parser/durchen/test_durchen.py
+++ tests/pecha/parser/durchen/test_durchen.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+from openpecha.pecha.layer import LayerEnum
 from openpecha.pecha.parsers.durchen import DurchenParser
 
 
@@ -16,15 +17,24 @@ def test_durchen():
     expected_base = (data / "expected_base.txt").read_text(encoding="utf-8")
     assert parser.base_text == expected_base
 
-    assert parser.anns == [
-        {
-            "Durchen": {"start": 0, "end": 5},
-            "note": "(1) <«ཅོ་»«སྡེ་»«སྣར་»«པེ་»༄༅། །ཆོས་ཀྱི་དབྱིངས་སུ་བསྟོད་པ། ༄༅༅། །>",
-        },
-        {"Durchen": {"start": 79, "end": 90}, "note": "(3) <«སྣར་»«པེ་»འཇམ་>"},
-        {"Durchen": {"start": 231, "end": 236}, "note": "(4) <«སྣར་»«པེ་»ལོ།>"},
-        {"Durchen": {"start": 540, "end": 544}, "note": "(5) <«སྣར་»«པེ་»རྣམས་>"},
+    # Check if parser has extract annotations in proper manner
+    expected_span_texts = ["༄༅། །", "འཕགས་པ་འཇམ་", "འདུད།", "རྣམ་"]
+    for ann, expected_span in zip(parser.anns, expected_span_texts):
+        start, end = (
+            ann[LayerEnum.durchen.value]["start"],
+            ann[LayerEnum.durchen.value]["end"],
+        )
+        assert parser.base_text[start:end] == expected_span
+
+    expected_ann_notes = [
+        "(1) <«ཅོ་»«སྡེ་»«སྣར་»«པེ་»༄༅། །ཆོས་ཀྱི་དབྱིངས་སུ་བསྟོད་པ། ༄༅༅། །>",
+        "(3) <«སྣར་»«པེ་»འཇམ་>",
+        "(4) <«སྣར་»«པེ་»ལོ།>",
+        "(5) <«སྣར་»«པེ་»རྣམས་>",
     ]
+
+    for ann, expected_note in zip(parser.anns, expected_ann_notes):
+        assert ann["note"] == expected_note
 
 
 test_durchen()
