3b29118684a44e9aec70845247ec5d80baaf4af5
tenzin3 <tenzintsunduebhattu@gmail.com>
2024-10-16 19:35:52 +0530

    get proper order for pechadb serializer

--- tests/pecha/serializers/pecha_db/test_pecha_db_serializer.py
+++ tests/pecha/serializers/pecha_db/test_pecha_db_serializer.py
@@ -9,7 +9,7 @@ def test_pecha_db_serializer():
     with tempfile.TemporaryDirectory() as tmpdirname:
         output_path = Path(tmpdirname)
         serializer = PechaDBSerializer(output_path=output_path)
-        pecha_path = Path("tests/pecha/serializers/pecha_db/data/IB1A4B480")
+        pecha_path = Path("tests/pecha/serializers/pecha_db/data/I08D84584")
         source_type = "dharmanexus"
         pecha_db_json_path, mapping_json_path = serializer.serialize(
             pecha_path=pecha_path,
@@ -21,3 +21,6 @@ def test_pecha_db_serializer():
         assert read_json(mapping_json_path) == read_json(
             "tests/pecha/serializers/pecha_db/data/expected_mapping.json"
         )
+
+
+test_pecha_db_serializer()
