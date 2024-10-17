3e39a015804819666d789880c965af256d1785a5
tenzin3 <tenzintsunduebhattu@gmail.com>
2024-10-17 11:13:56 +0530

    restructure dharmanexus serializer test files

--- tests/pecha/serializers/pecha_db/dharmanexus/test_pecha_db_serializer.py
+++ tests/pecha/serializers/pecha_db/dharmanexus/test_pecha_db_serializer.py
@@ -9,17 +9,17 @@ def test_pecha_db_serializer():
     with tempfile.TemporaryDirectory() as tmpdirname:
         output_path = Path(tmpdirname)
         serializer = PechaDBSerializer(output_path=output_path)
-        pecha_path = Path("tests/pecha/serializers/pecha_db/data/I08D84584")
+        pecha_path = Path("tests/pecha/serializers/pecha_db/dharmanexus/data/I08D84584")
         source_type = "dharmanexus"
         pecha_db_json_path, mapping_json_path = serializer.serialize(
             pecha_path=pecha_path,
             source_type=source_type,
         )
         assert read_json(pecha_db_json_path) == read_json(
-            "tests/pecha/serializers/pecha_db/data/expected_pecha_db.json"
+            "tests/pecha/serializers/pecha_db/dharmanexus/data/expected_pecha_db.json"
         )
         assert read_json(mapping_json_path) == read_json(
-            "tests/pecha/serializers/pecha_db/data/expected_mapping.json"
+            "tests/pecha/serializers/pecha_db/dharmanexus/data/expected_mapping.json"
         )
 
 
