2409806b84bfb75bc1adc93e3d4cc487aba51dd5
tenzin3 <tenzintsunduebhattu@gmail.com>
2024-12-02 14:55:57 +0530

    delete unneccessary line

--- tests/pecha/serializers/pecha_db/commentary/test_commentary_serializer.py
+++ tests/pecha/serializers/pecha_db/commentary/test_commentary_serializer.py
@@ -35,6 +35,3 @@ def test_commentary_serializer():
 
         expected_serialized_json = read_json(DATA_DIR / "commentary_serialized.json")
         assert serialized_json == expected_serialized_json
-
-
-test_commentary_serializer()
