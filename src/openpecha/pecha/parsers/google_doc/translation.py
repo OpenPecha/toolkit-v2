cdbdc686abe70471f12c337d46763eb7c9136a13
tenzin3 <tenzintsunduebhattu@gmail.com>
2024-12-11 10:10:03 +0530

    add test data

---
+++ src/openpecha/pecha/parsers/google_doc/translation.py
@@ -0,0 +1,12 @@
+from openpecha.pecha.parsers import BaseParser
+
+
+class GoogleDocTranslationParser(BaseParser):
+    def __init__(self):
+        pass
+
+    def parse_bo(self):
+        pass
+
+    def parse_bo_translation(self):
+        pass
