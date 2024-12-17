6c50dc589afbd2b215740b0e1c9ee9ce51c2f40d
tenzin3 <tenzintsunduebhattu@gmail.com>
2024-12-17 14:40:59 +0530

    delete unneccessary files

--- tests/pecha/parser/google_doc/commentary/test_commentary.py
+++ tests/pecha/parser/google_doc/commentary/test_commentary.py
@@ -35,46 +35,10 @@ def test_parser_on_root_text():
             assert parser.base[start:end] == seg
 
 
-def test_parser_on_commentary_text():
+def test_parser_on_commentary():
     data = Path(__file__).parent / "data"
-    input = data / "commentary/dolma_21.docx"
+    input = data / "commentary/རྡོ་རྗེ་གཅོད་པ་_commentary.docx"
     metadata = read_json(data / "commentary/metadata.json")
-
-    parser = GoogleDocCommentaryParser(
-        source_type="commentary", root_path="opf_id/layers/basename/layer_file.json"
-    )
-
-    with tempfile.TemporaryDirectory() as tmpdirname:
-        output_path = Path(tmpdirname)
-
-        parser.parse(input, metadata, output_path)
-
-        expected_base = (data / "commentary/expected_base.txt").read_text(
-            encoding="utf-8"
-        )
-        assert parser.base == expected_base
-
-        expected_segments = [
-            "སྒྲོལ་མ་ཉེར་གཅིག་པའི་བསྟོད་འགྲེལ་འཕྲིན་ལས་ཆར་དུ་སྙིལ་བའི་སྤྲིན་ཕུང་།",
-            "དང་པོ་ནི། རྒྱ་གར་སྐད་དུ། ན་མཿ ཏཱརཱ་ཨེ་ཀ་བིཾ་ཤ་ཏི་སྟོ་ཏྲ་གུ་ཎ་ཧི་ཏ་སཱ་ཀ །བོད་སྐད་དུ། སྒྲོལ་མ་ལ་ཕྱག་འཚལ་ཉི་ཤུ་རྩ་གཅིག་གིས་བསྟོད་པ་ཕན་ཡོན་དང་བཅས་པ། ཞེས་པའི་དོན་ཏོ། །",
-            "གཉིས་པ་ནི། རྗེ་བཙུན་མ་འཕགས་མ་སྒྲོལ་མ་ལ་ཕྱག་འཚལ་ལོ། ། ཞེས་གསུངས།",
-            "གསུམ་པ་ལ་ཕྱག་འཚལ་ཉེར་གཅིག་ཡོད་པ་ལས། ཕྱག་འཚལ་དང་པོ་རྗེ་བཙུན་མ་འཕགས་མ་སྒྲོལ་མ་ལ་ཕྱག་འཚལ་བ་ནི།\nརྒྱུད་ལས།།\nཕྱག་འཚལ་སྒྲོལ་མ་མྱུར་མ་དཔའ་མོ། །\nསྤྱན་ནི་སྐད་གཅིག་གློག་དང་འདྲ་མ། །\nའཇིག་རྟེན་གསུམ་མགོན་ཆུ་སྐྱེས་ཞལ་གྱི། །\nགེ་སར་བྱེ་བ་ལས་ནི་བྱུང་མ། །\nཞེས་གསུངས་པའི་དོན་ནི།",
-            "ཕྱག་འཚལ་ལོ་རྗེ་བཙུན་མ་འཕགས་མ་སྒྲོལ་མ་ལའོ། །རྒྱུ་གང་གི་སྒོ་ནས་ཕྱག་འཚལ་ཞེ་ན། སྒྲོལ་མྱུར་དཔའ་གསུམ་གྱི་ཆེ་བའི་སྒོ་ནས་ཏེ། དང་པོ་སྒྲོལ་མ་ཞེས་པའི་མཚན་གྱི་ཆེ་བ་ནི། ལྷ་མོ་འདིས་སེམས་ཅན་དཔག་ཏུ་མེད་པ་གནས་སྐབས་འཇིགས་པ་བརྒྱད་སོགས་དང་། མཐར་ཉོན་མོངས་པ་དང་ཤེས་བྱའི་སྒྲིབ་པ་ལས་བསྒྲལ་ཏེ་བླ་མེད་རྫོགས་པའི་སངས་རྒྱས་ཀྱི་སར་བཀོད་པས་ན། བསྐལ་པ་དཔག་ཏུ་མེད་པའི་སྔོན་རོལ་ནས་སྒྲོལ་མ་ཞེས་པའི་མཚན་གྱིས་བསྔགས་པ་ཡིན་ཏེ།",
-            "སྐབས་འདིའི་འོད་ཟེར་ནི། འགྲེལ་པ་འགའ་ཞིག་ལས། ཕྱག་གཡས་པའི་མཐིལ་གྱི་འཁོར་ལོའི་འོད་ཟེར་ཡིན་པར་གསུངས་པ་དང་། གཞན་དག་སྐུའི་འོད་ཟེར་ཡིན་པར་གསུངས་པ་ས་བཞེད་པ་མི་འདྲ་ཡང་།",
-            "སྐབས་འདིའི་འོད་ཟེར་ནི། འགྲེལ་པ་འགའ་ཞིག་ལས། ཕྱག་གཡས་པའི་མཐིལ་གྱི་འཁོར་ལོའི་འོད་ཟེར་ཡིན་པར་གསུངས་པ་དང་། གཞན་དག་སྐུའི་འོད་ཟེར་ཡིན་པར་གསུངས་པ་ས་བཞེད་པ་མི་འདྲ་ཡང་།",
-        ]
-        for ann, seg in zip(parser.meaning_segment_anns, expected_segments):
-            start, end = (
-                ann[LayerEnum.meaning_segment.value]["start"],
-                ann[LayerEnum.meaning_segment.value]["end"],
-            )
-            assert parser.base[start:end] == seg
-
-
-def test_parser_on_commentary_with_sapche():
-    data = Path(__file__).parent / "data"
-    input = data / "commentary_with_sapche/རྡོ་རྗེ་གཅོད་པ་_commentary.docx"
-    metadata = read_json(data / "commentary_with_sapche/metadata.json")
 
     parser = GoogleDocCommentaryParser(
         source_type="commentary", root_path="opf_id/layers/basename/layer_file.json"
@@ -91,6 +55,3 @@ def test_parser_on_commentary_with_sapche():
     ]
 
     assert parser.sapche_anns == expected_sapche_anns
-
-
-test_parser_on_commentary_with_sapche()
