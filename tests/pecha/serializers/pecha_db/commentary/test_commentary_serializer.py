afe05ac8227177abc5066ee0e9d59ffb562628fe
tenzin3 <tenzintsunduebhattu@gmail.com>
2024-12-18 10:06:31 +0530

    add en commentary serializer test data

--- tests/pecha/serializers/pecha_db/commentary/test_commentary_serializer.py
+++ tests/pecha/serializers/pecha_db/commentary/test_commentary_serializer.py
@@ -34,3 +34,35 @@ def test_bo_commentary_serializer():
 
         expected_serialized_json = read_json(DATA_DIR / "bo/commentary_serialized.json")
         assert serialized_json == expected_serialized_json
+
+
+def test_en_commentary_serializer():
+    pecha_path = DATA_DIR / "en/I088F7504"
+
+    # Patch the `get_category` method in `CategoryExtractor` to return a custom value
+    with patch(
+        "pecha_org_tools.extract.CategoryExtractor.get_category"
+    ) as mock_get_category:
+        mock_get_category.return_value = {
+            "bo": [
+                {"name": "སངས་རྒྱས་ཀྱི་བཀའ།", "heDesc": "", "heShortDesc": ""},
+                {"name": "རྡོ་རྗེ་གཅོད་པ།", "heDesc": "", "heShortDesc": ""},
+                {"name": "འགྲེལ་པ།", "heDesc": "", "heShortDesc": ""},
+                {"name": "རྡོ་རྗེ་གཅོད་པ།", "heDesc": "", "heShortDesc": ""},
+            ],
+            "en": [
+                {"name": "The Buddha's Teachings", "enDesc": "", "enShortDesc": ""},
+                {"name": "Vajra Cutter", "enDesc": "", "enShortDesc": ""},
+                {"name": "Commentaries", "enDesc": "", "enShortDesc": ""},
+                {"name": "Vajra Cutter", "enDesc": "", "enShortDesc": ""},
+            ],
+        }
+
+        serializer = CommentarySerializer()
+        serialized_json = serializer.serialize(pecha_path, "Vajra Cutter")
+
+        expected_serialized_json = read_json(DATA_DIR / "en/commentary_serialized.json")
+        assert serialized_json == expected_serialized_json
+
+
+test_en_commentary_serializer()
