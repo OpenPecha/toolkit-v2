50ea637b10429b053b37cc3d42f9f44048d5231f
tenzin3 <tenzintsunduebhattu@gmail.com>
2024-12-20 09:49:36 +0530

    new test translation serializer

--- tests/alignment/serializers/translation/test_translation_serializer.py
+++ tests/alignment/serializers/translation/test_translation_serializer.py
@@ -4,24 +4,19 @@ from unittest import TestCase
 from openpecha.alignment.serializers.simple_text_translation import (
     SimpleTextTranslationSerializer,
 )
+from openpecha.utils import read_json
 
 DATA_DIR = Path(__file__).parent / "data"
 OUTPUT_DIR = Path(__file__).parent / "output"
 
 
-class TestTranslationSerializer(TestCase):
-    def setUp(self):
-        pass
-
-    def serialize(self):
+class TestSimpleTextTranslationSerializer(TestCase):
+    def test_translation_serializer(self):
         root_opf = DATA_DIR / "bo/IFA46BBC2"
         translation_opf = DATA_DIR / "en/I6EA29D09"
 
         serializer = SimpleTextTranslationSerializer()
-        serializer.serialize(root_opf, translation_opf, OUTPUT_DIR)
+        json_output = serializer.serialize(root_opf, translation_opf, OUTPUT_DIR)
 
-    def tearDown(self):
-        pass
-
-
-TestTranslationSerializer().serialize()
+        expected_json_path = DATA_DIR / "expected_output.json"
+        assert read_json(json_output) == read_json(expected_json_path)
