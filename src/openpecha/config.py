2859136c96cdbf586a1458a0a52621e9ecaffb60
ta4tsering <ta3tsering@gmail.com>
2025-01-03 14:58:31 +0530

    updated the google api and added credentials path in config

--- src/openpecha/config.py
+++ src/openpecha/config.py
@@ -15,6 +15,7 @@ def _mkdir_if_not(path: Path):
         path.mkdir(exist_ok=True, parents=True)
     return path
 
+GOOGLE_API_CRENDENTIALS_PATH = Path("~/.gcloud/google_docs_and_sheets.json").expanduser()
 
 BASE_PATH = _mkdir_if_not(Path.home() / ".openpecha")
 PECHAS_PATH = _mkdir_if_not(BASE_PATH / "pechas")
--- src/openpecha/pecha/parsers/google_doc/google_api.py
+++ src/openpecha/pecha/parsers/google_doc/google_api.py
@@ -1,39 +1,49 @@
-import os
-import tempfile
 from pathlib import Path
-from typing import Optional
+from urllib.parse import urlparse
+from typing import Optional, Union
 
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from googleapiclient.http import MediaIoBaseDownload
-
-
-tempdir = tempfile.TemporaryDirectory()
-OUTPUT_DIR = Path(tempdir.name)
-
-
-credentials_path = Path("~/.gcloud/google_docs_and_sheets.json").expanduser()
-# os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = str(credentials_path)
+from openpecha.config import PECHAS_PATH, GOOGLE_API_CRENDENTIALS_PATH
 
 
 class GoogleDocAndSheetsDownloader:
     def __init__(
         self,
-        google_docs_id: Optional[str] = None,
-        google_sheets_id: Optional[str] = None,
-    ):
+        google_docs_link: Optional[str] = None,
+        google_sheets_link: Optional[str] = None,
+        output_dir: Union[str, Path] = PECHAS_PATH,
+    ):  
+        self.output_dir = Path(output_dir)
         self.credentials = service_account.Credentials.from_service_account_file(
-            credentials_path,
+            GOOGLE_API_CRENDENTIALS_PATH,
             scopes=["https://www.googleapis.com/auth/drive.readonly"],
         )
         self.drive_service = build("drive", "v3", credentials=self.credentials)
 
-        if google_docs_id is not None:
+        if google_docs_link is not None:
+            google_docs_id = self.get_id_from_link(google_docs_link)
             self.docx_path = self.get_google_docs(google_docs_id)
 
-        if google_sheets_id is not None:
+        if google_sheets_link is not None:
+            google_sheets_id = self.get_id_from_link(google_sheets_link)
             self.sheets_path = self.get_google_sheets(google_sheets_id)
+
+
+    def get_id_from_link(self, link: str) -> str:
+        parsed_url = urlparse(link)
+        path_segments = parsed_url.path.split('/')
+        try:
+            d_index = path_segments.index('d')
+            doc_id = path_segments[d_index + 1]
+            if not doc_id:
+                raise ValueError("Document ID is empty.")
+            return doc_id
+        except (ValueError, IndexError) as e:
+            raise ValueError(f"Invalid Google Docs link format: {link}") from e
+
 
 
     def get_google_docs(self, google_docs_id: str) -> Optional[Path]:
@@ -42,7 +52,7 @@ class GoogleDocAndSheetsDownloader:
                 fileId=google_docs_id, fields="name"
             ).execute()
             document_title = file_metadata.get("name", "untitled_document")
-            docx_path = OUTPUT_DIR / f"{document_title}.docx"
+            docx_path = self.output_dir / f"{document_title}.docx"
 
             mime_type = "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
             request = self.drive_service.files().export_media(
@@ -67,16 +77,12 @@ class GoogleDocAndSheetsDownloader:
                 fileId=google_sheets_id, fields="name"
             ).execute()
             sheet_title = file_metadata.get("name", "untitled_sheet")
-            # with tempfile.TemporaryDirectory() as tmpdirname:
-            #     OUTPUT_DIR = Path(tmpdirname)
-            sheet_path = OUTPUT_DIR / f"{sheet_title}.xlsx"
-
+            sheet_path = self.output_dir / f"{sheet_title}.xlsx"
             # Prepare the export request
             mime_type = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
             request = self.drive_service.files().export_media(
                 fileId=google_sheets_id, mimeType=mime_type
             )
-
             # Download the file
             with sheet_path.open("wb") as fh:
                 downloader = MediaIoBaseDownload(fh, request)
@@ -85,34 +91,6 @@ class GoogleDocAndSheetsDownloader:
                     _, done = downloader.next_chunk()
 
             return sheet_path
-        except HttpError as error:
-            print(f"An error occurred: {error}")
-            return None
-        
-
-class GoogleDocAndSheets:
-    def __init__(self,google_docs_id: Optional[str] = None,google_sheets_id: Optional[str] = None):
-        if google_docs_id is not None:
-            self.docx_path = self.get_google_docs(google_docs_id)
-        if google_sheets_id is not None:
-            self.sheets_path = self.get_google_sheets(google_sheets_id)
-
-
-    def get_google_docs(self, google_docs_id):
-        try:
-            docs_service = build('docs', 'v1')
-            document = docs_service.documents().get(documentId=google_docs_id).execute()
-            return document
-        except HttpError as error:
-            print(f"An error occurred: {error}")
-            return None
-
-
-    def get_google_sheets(self, google_sheets_id: str):
-        try:
-            sheets_service = build('sheets', 'v4')
-            sheet = sheets_service.spreadsheets().get(spreadsheetId=google_sheets_id).execute()
-            return sheet
         except HttpError as error:
             print(f"An error occurred: {error}")
             return None
