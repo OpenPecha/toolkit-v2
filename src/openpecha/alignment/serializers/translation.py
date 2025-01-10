5be8a8b3f12e6506ef68d84f0a1b9963d9f7fde7
tenzin3 <tenzintsunduebhattu@gmail.com>
2025-01-10 12:25:27 +0530

    get pecha title from lang

--- src/openpecha/alignment/serializers/translation.py
+++ src/openpecha/alignment/serializers/translation.py
@@ -235,7 +235,8 @@ class TextTranslationSerializer(BaseAlignmentSerializer):
         self.set_translation_content(is_pecha_display)
 
         # Write the JSON to the output path
-        json_fname = f"{pecha_title}_{Pecha.from_path(self.translation_opf_path).metadata.title}.json"
+        translation_pecha_title = self.get_pecha_title(self.translation_opf_path)
+        json_fname = f"{pecha_title}_{translation_pecha_title}.json"
         json_output_path = output_path / json_fname
         _mkdir_if_not(output_path)
         json_output = {
