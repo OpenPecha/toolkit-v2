e4a97b5e608f8bf056898616a55b8ff9933d1351
tenzin3 <tenzintsunduebhattu@gmail.com>
2025-01-23 14:45:59 +0000

    fix/take authenticated token directly

--- src/openpecha/pecha/parsers/google_doc/google_api.py
+++ src/openpecha/pecha/parsers/google_doc/google_api.py
@@ -4,48 +4,13 @@ from pathlib import Path
 from typing import Optional
 from urllib.parse import urlparse
 
-from google.auth.transport.requests import Request
-from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseDownload
 
-from openpecha.config import GOOGLE_API_CRENDENTIALS_PATH, GOOGLE_API_TOKEN_PATH
+from openpecha.config import GOOGLE_API_TOKEN_PATH
 
 
 class GoogleDocAndSheetsDownloader:
-    def __init__(
-        self,
-        credential_path: str = GOOGLE_API_CRENDENTIALS_PATH,
-    ):
-        self.credential_path = credential_path
-        self.token = self.authenticate()
-
-    def authenticate(self):
-        """
-        Authenticate the user using the credentail json file.
-        If not authenticated, it will open a browser to authenticate the user.And save the token in the token.pickle file.
-
-        If yes, it will load the token from the token.pickle file.
-        """
-        SCOPES = ["https://www.googleapis.com/auth/drive.readonly"]
-
-        creds = None
-        if GOOGLE_API_TOKEN_PATH.exists():
-            with open(GOOGLE_API_TOKEN_PATH, "rb") as token:
-                creds = pickle.load(token)
-        if not creds or not creds.valid:
-            if creds and creds.expired and creds.refresh_token:
-                creds.refresh(Request())
-            else:
-                flow = InstalledAppFlow.from_client_secrets_file(
-                    self.credential_path, SCOPES
-                )
-                creds = flow.run_local_server(port=0)
-            with open(GOOGLE_API_TOKEN_PATH, "wb") as token:
-                pickle.dump(creds, token)
-
-        self.token = creds
-
     def get_id_from_link(self, link: str) -> Optional[str]:
         """
         Input: link to a Google Doc or Google sheet Document.
@@ -60,17 +25,16 @@ class GoogleDocAndSheetsDownloader:
                 raise ValueError("Document ID is empty.")
             return id
         except Exception as e:
-            raise ValueError(f"[Invalid link]:[{link}]{str(e)}")
+            raise ValueError(f"[Invalid link] [{link}]{str(e)}")
 
     def get_google_docx(self, file_link: str, output_path: Path):
+        with open(GOOGLE_API_TOKEN_PATH, "rb") as token:
+            cred = pickle.load(token)
+
         file_id = self.get_id_from_link(file_link)
         try:
-            service = build("drive", "v3", credentials=self.token)
-            request = service.files().export_media(
-                fileId=file_id,
-                mimeType="application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-                supportsAllDrives=True,
-            )
+            service = build("drive", "v3", credentials=cred)
+            request = service.files().get_media(fileId=file_id, supportsAllDrives=True)
             fh = io.BytesIO()
             downloader = MediaIoBaseDownload(fh, request)
             done = False
@@ -86,9 +50,12 @@ class GoogleDocAndSheetsDownloader:
             print(f"An error occurred: {e}")
 
     def get_google_sheet(self, file_link: str, output_path: Path):
+        with open(GOOGLE_API_TOKEN_PATH, "rb") as token:
+            cred = pickle.load(token)
+
         file_id = self.get_id_from_link(file_link)
         try:
-            service = build("drive", "v3", credentials=self.token)
+            service = build("drive", "v3", credentials=cred)
             request = service.files().export_media(
                 fileId=file_id,
                 mimeType="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
