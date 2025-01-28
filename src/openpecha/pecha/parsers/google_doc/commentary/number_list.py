2ff89136df220708c9350defb445095c7c49f481
tenzin3 <tenzintsunduebhattu@gmail.com>
2025-01-28 04:34:52 +0000

    extract number list from extracted docx text

--- src/openpecha/pecha/parsers/google_doc/commentary/number_list.py
+++ src/openpecha/pecha/parsers/google_doc/commentary/number_list.py
@@ -1,6 +1,59 @@
+import re
+from pathlib import Path
+from typing import Any, Dict, Optional
+
+from docx2python import docx2python
+
+from openpecha.config import PECHAS_PATH
 from openpecha.pecha.parsers import BaseParser
 
 
-class GoogleDocNumberListCommentaryParser(BaseParser):
-    def __init__(self):
-        pass
+class DocxNumberListCommentaryParser(BaseParser):
+    def __init__(self, root_path: Optional[str] = None):
+        self.root_path = root_path
+        self.number_list_regex = r"^(\d+)\)\t(.*)"
+
+    def normalize_text(self, text: str):
+        text = self.normalize_whitespaces(text)
+        text = self.normalize_newlines(text)
+        text = text.strip()
+        return text
+
+    @staticmethod
+    def normalize_whitespaces(text: str):
+        """
+        If there are spaces or tab between newlines, it will be removed.
+        """
+        return re.sub(r"\n[\s\t]+\n", "\n\n", text)
+
+    @staticmethod
+    def normalize_newlines(text: str):
+        """
+        If there are more than 2 newlines continuously, it will replace it with 2 newlines.
+        """
+        return re.sub(r"\n{3,}", "\n\n", text)
+
+    def get_number_list(self, text: str) -> Dict:
+        """
+        Extract number list from the extracted text from docx.
+        """
+        res: Dict[str, str] = {}
+        for para_text in text.split("\n\n"):
+            match = re.match(self.number_list_regex, para_text)
+            if match:
+                number = match.group(1)
+                text = match.group(2)
+                res[number] = text
+
+        return res
+
+    def parse(
+        self,
+        input: Path,
+        metadata: Dict[str, Any],
+        output_path: Path = PECHAS_PATH,
+    ):
+        text = docx2python(input).text
+        text = self.normalize_text(text)
+        res = self.get_number_list(text)
+        return res
--- tests/pecha/parser/google_doc/commentary/number_list/test_number_list.py
+++ tests/pecha/parser/google_doc/commentary/number_list/test_number_list.py
@@ -1,15 +1,32 @@
 from pathlib import Path
 from unittest import TestCase
 
+from openpecha.pecha.parsers.google_doc.commentary.number_list import (
+    DocxNumberListCommentaryParser,
+)
+from openpecha.pecha.parsers.parser_utils import extract_metadata_from_xlsx
+
 
 class TestNumberListCommentaryParser(TestCase):
     def setUp(self):
         self.data_dir = Path(__file__).parent / "data"
-        self.bo_docx_file = self.data_dir / "དབུ་མ་_bo_commentary.docx"
-        self.bo_metadata_file = self.data_dir / "དབུ་མ་_bo_commentary_metadata.xlsx"
+        self.input = self.data_dir / "དབུ་མ་_bo_commentary.docx"
+        self.metadata = extract_metadata_from_xlsx(
+            self.data_dir / "དབུ་མ་_bo_commentary_metadata.xlsx"
+        )
+        self.output_dir = self.data_dir / "output"
 
     def test_parse_numberlist_commentary_parser(self):
-        pass
+        parser = DocxNumberListCommentaryParser(
+            root_path="opf_id/layers/basename/layer_file.json"
+        )
+        parser.parse(self.input, self.metadata, self.output_dir)
 
     def tearDown(self):
         pass
+
+
+parser = TestNumberListCommentaryParser()
+parser.setUp()
+parser.test_parse_numberlist_commentary_parser()
+parser.tearDown()
