2642f24bb724ee5ceda5cdd29bcf4df03eb76559
tenzin3 <tenzintsunduebhattu@gmail.com>
2025-02-12 05:41:30 +0000

    add exception classes

--- src/openpecha/exceptions.py
+++ src/openpecha/exceptions.py
@@ -32,3 +32,51 @@ class GithubCloneError(GithubRepoError):
     """Raised when there is an error cloning github repo"""
 
     pass
+
+
+class GithubRepoNotFoundError(GithubRepoError):
+    """Raised when the specified github repo is not found"""
+
+    pass
+
+
+class FileNotFoundError(Exception):
+    """Raised when the specified file is not found."""
+
+    pass
+
+
+class EmptyFileError(Exception):
+    """Raised when the specified file is empty."""
+
+    pass
+
+
+class MetaDataMissingError(Exception):
+    """Raised when the metadata is missing in the file."""
+
+    pass
+
+
+class MetaDataValidationError(Exception):
+    """Raised when the metadata is not valid."""
+
+    pass
+
+
+class PechaCategoryNotFoundError(Exception):
+    """Raised when the specified pecha category is not found."""
+
+    pass
+
+
+class BaseUpdateFailedError(Exception):
+    """Raised when the base update mechanism failed."""
+
+    pass
+
+
+class AlignmentAnnotationTransferFailedError(Exception):
+    """Raised when the alignment annotation transfer failed."""
+
+    pass
