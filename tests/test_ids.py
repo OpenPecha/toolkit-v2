da9cdedbd5ae1e274e85c7858b723dc5722cbd0f
gangagyatso4364 <gyatsokhan@gmail.com>
2025-02-17 14:25:34 +0530

    added get layerd id function test

--- tests/test_ids.py
+++ tests/test_ids.py
@@ -42,7 +42,7 @@ def test_get_layer_id():
     layer_id = get_layer_id()
     assert re.match(
         r"^[0-9A-F]{4}$", layer_id
-    ), f"Base ID {layer_id} is not in the correct format"
+    ), f"Layer ID {layer_id} is not in the correct format"
 
 
 def test_get_initial_pecha_id():
