24476e6dd0b2834e976f776db8fc52cf4d8610ac
gangagyatso4364 <gyatsokhan@gmail.com>
2025-02-17 14:04:59 +0530

    added get layerd id function test

--- tests/test_ids.py
+++ tests/test_ids.py
@@ -8,6 +8,7 @@ from openpecha.ids import (
     get_diplomatic_id,
     get_id,
     get_initial_pecha_id,
+    get_layer_id,
     get_open_pecha_id,
     get_uuid,
     get_work_id,
@@ -32,6 +33,13 @@ def test_get_id():
 
 def test_get_base_id():
     base_id = get_base_id()
+    assert re.match(
+        r"^[0-9A-F]{4}$", base_id
+    ), f"Base ID {base_id} is not in the correct format"
+
+
+def test_get_layer_id():
+    base_id = get_layer_id()
     assert re.match(
         r"^[0-9A-F]{4}$", base_id
     ), f"Base ID {base_id} is not in the correct format"
