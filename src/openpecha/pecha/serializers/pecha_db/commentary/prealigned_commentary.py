2a29bc10e754718d793c6bb62e4866460fb77831
tenzin3 <tenzintsunduebhattu@gmail.com>
2025-03-05 10:51:26 +0000

    PreAlignedCommentarySerializer serialize

--- src/openpecha/pecha/serializers/pecha_db/commentary/prealigned_commentary.py
+++ src/openpecha/pecha/serializers/pecha_db/commentary/prealigned_commentary.py
@@ -1,15 +1,106 @@
 from pathlib import Path
-from typing import Dict, List
+from typing import Any, Dict, List
 
+from pecha_org_tools.extract import CategoryExtractor
 from stam import AnnotationStore
 
 from openpecha.config import get_logger
+from openpecha.exceptions import MetaDataValidationError, PechaCategoryNotFoundError
 from openpecha.pecha import Pecha, get_anns
+from openpecha.pecha.metadata import PechaMetaData
+from openpecha.utils import get_text_direction_with_lang
 
 logger = get_logger(__name__)
 
 
 class PreAlignedCommentarySerializer:
+    def extract_metadata(self, pecha: Pecha):
+        """
+        Extract neccessary metadata from opf for serialization to json
+        """
+        metadata: PechaMetaData = pecha.metadata
+
+        if not isinstance(metadata.title, dict):
+            logger.error(f"Title is not available in the Commentary Pecha {pecha.id}.")
+            raise MetaDataValidationError(
+                f"[Error] Commentary Pecha {pecha.id} has no English or Tibetan Title."
+            )
+
+        pecha_lang = pecha.metadata.language.value
+        src_lang = "en" if pecha_lang == "bo" else pecha_lang
+        source_title = metadata.title.get(src_lang.lower()) or metadata.title.get(
+            src_lang.upper()
+        )
+        source_title = (
+            source_title if src_lang == "en" else f"{source_title}[{src_lang}]"
+        )
+        target_lang = "bo"
+        target_title = metadata.title.get(target_lang.lower()) or metadata.title.get(
+            target_lang.upper()
+        )
+
+        src_metadata = {
+            "title": source_title,
+            "language": src_lang,
+            "versionSource": metadata.source if metadata.source else "",
+            "direction": get_text_direction_with_lang("en"),
+            "completestatus": "done",
+        }
+
+        tgt_metadata = {
+            "title": target_title,
+            "language": target_lang,
+            "versionSource": metadata.source if metadata.source else "",
+            "direction": get_text_direction_with_lang("bo"),
+            "completestatus": "done",
+        }
+
+        return src_metadata, tgt_metadata
+
+    def get_category(self, category_name: str):
+        """
+        Input: title: Title of the pecha commentary which will be used to get the category format
+        Process: Get the category format from the pecha.org categorizer package
+        """
+
+        try:
+            categorizer = CategoryExtractor()
+            category = categorizer.get_category(category_name)
+        except Exception as e:
+            logger.error(
+                f"Category not found for pecha title: {category_name}. {str(e)}"
+            )
+            raise PechaCategoryNotFoundError(
+                f"Category not found for pecha title: {category_name}. {str(e)}"
+            )
+        return category
+
+    def add_root_reference_to_category(self, category: Dict[str, Any], root_title: str):
+        """
+        Modify the category format to the required format for pecha.org commentary
+        """
+        for lang in ["bo", "en"]:
+            last_category = category[lang][-1]
+            last_category.update(
+                {
+                    "base_text_titles": [root_title],
+                    "base_text_mapping": "many_to_one",
+                    "link": "Commentary",
+                }
+            )
+        return category
+
+    def get_categories(self, pecha: Pecha, root_title: str):
+        """
+        Set the category format to self.category attribute
+        """
+
+        title = pecha.metadata.title.get("bo") or pecha.metadata.title.get("BO")
+        category = self.get_category(title)
+        category = self.add_root_reference_to_category(category, root_title)
+
+        return (category["en"], category["bo"])  # source and target category
+
     def get_first_layer_path(self, pecha: Pecha) -> Path:
         return next(pecha.layer_path.rglob("*.json"))
 
@@ -112,7 +203,7 @@ class PreAlignedCommentarySerializer:
 
         commentary_layer_path = self.get_first_layer_path(commentary_pecha)
         commentary_anns = get_anns(AnnotationStore(file=str(commentary_layer_path)))
-        segments = []
+        serialized_content = []
         for ann in commentary_anns:
             root_indices = parse_root_mapping(ann["root_idx_mapping"])
             root_idx = root_indices[0]
@@ -127,7 +218,7 @@ class PreAlignedCommentarySerializer:
             idx_not_in_root = root_idx not in root_anns
             is_root_empty = is_empty(root_anns[root_idx]["text"])
             if is_commentary_empty or idx_not_in_root or is_root_empty:
-                segments.append(commentary_text)
+                serialized_content.append(commentary_text)
                 continue
 
             # Dont include mapping if root_display is empty
@@ -137,11 +228,61 @@ class PreAlignedCommentarySerializer:
                 root_display_anns[root_display_idx]["text"]
             )
             if idx_not_in_root_display or is_root_display_empty:
-                segments.append(commentary_text)
+                serialized_content.append(commentary_text)
                 continue
 
-            segments.append(f"<1><{root_display_idx}>{commentary_text}")
-        return segments
+            serialized_content.append(f"<1><{root_display_idx}>{commentary_text}")
+        return serialized_content
+
+    def get_pecha_en_title(self, pecha: Pecha):
+        metadata: PechaMetaData = pecha.metadata
+
+        if not isinstance(metadata.title, dict):
+            logger.error(
+                f"Title data type is not dictionary in the Root Pecha {pecha.id}."
+            )
+            raise MetaDataValidationError(
+                f"[Error] Root Pecha {pecha.id} title data type is not dictionary."
+            )
+
+        if "en" not in metadata.title and "EN" not in metadata.title:
+            logger.error(
+                f"English title is not available in the Root Pecha {pecha.id}."
+            )
+            raise MetaDataValidationError(
+                f"[Error] Root Pecha {pecha.id} has no English Title."
+            )
+
+        root_en_title = metadata.title.get("en") or metadata.title.get("EN")
+        return root_en_title
+
+    def serialize(
+        self, root_display_pecha: Pecha, root_pecha: Pecha, commentary_pecha: Pecha
+    ):
+        src_book, tgt_book = [], []
+        src_metadata, tgt_metadata = self.extract_metadata(commentary_pecha)
+        src_book.append(src_metadata)
+        tgt_book.append(tgt_metadata)
+
+        root_en_title = self.get_pecha_en_title(root_display_pecha)
+        src_category, tgt_category = self.get_categories(
+            commentary_pecha, root_en_title
+        )
+
+        src_content: List[List[str]] = []
+        tgt_content = self.get_serialized_commentary(
+            root_display_pecha, root_pecha, commentary_pecha
+        )
+
+        src_book[0]["content"] = src_content
+        tgt_book[0]["content"] = tgt_content
+
+        serialized_json = {
+            "source": {"categories": src_category, "books": src_book},
+            "target": {"categories": tgt_category, "books": tgt_book},
+        }
+        logger.info(f"Pecha {commentary_pecha.id} is serialized successfully.")
+        return serialized_json
 
 
 def parse_root_mapping(mapping: str) -> List[int]:
--- tests/pecha/serializers/pecha_db/commentary/prealigned_simple/test_prealigned_simple_commentary.py
+++ tests/pecha/serializers/pecha_db/commentary/prealigned_simple/test_prealigned_simple_commentary.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from unittest import TestCase
+from unittest import TestCase, mock
 
 from openpecha.pecha import Pecha
 from openpecha.pecha.serializers.pecha_db.commentary.prealigned_commentary import (
@@ -15,6 +15,27 @@ class TestPreAlignedCommentarySerializer(TestCase):
         self.root_display_pecha = Pecha.from_path(self.DATA_DIR / "P1/IA6E66F92")
         self.commentary_pecha = Pecha.from_path(self.DATA_DIR / "P3/I77BD6EA9")
 
+        # Create the patcher and set return_value
+        self.patcher = mock.patch(
+            "openpecha.pecha.serializers.pecha_db.commentary.prealigned_commentary.PreAlignedCommentarySerializer.get_category",
+            return_value={
+                "bo": [
+                    {"name": "སངས་རྒྱས་ཀྱི་བཀའ།", "heDesc": "", "heShortDesc": ""},
+                    {"name": "རྡོ་རྗེ་གཅོད་པ།", "heDesc": "", "heShortDesc": ""},
+                    {"name": "འགྲེལ་པ།", "heDesc": "", "heShortDesc": ""},
+                    {"name": "རྡོ་རྗེ་གཅོད་པ།", "heDesc": "", "heShortDesc": ""},
+                ],
+                "en": [
+                    {"name": "The Buddha's Teachings", "enDesc": "", "enShortDesc": ""},
+                    {"name": "Vajra Cutter", "enDesc": "", "enShortDesc": ""},
+                    {"name": "Commentaries", "enDesc": "", "enShortDesc": ""},
+                    {"name": "Vajra Cutter", "enDesc": "", "enShortDesc": ""},
+                ],
+            },
+        )
+        # Start the patch
+        self.mock_get_category = self.patcher.start()
+
     def test_get_serialized_commentary(self):
         serializer = PreAlignedCommentarySerializer()
         serialized_json = serializer.get_serialized_commentary(
@@ -24,11 +45,14 @@ class TestPreAlignedCommentarySerializer(TestCase):
         expected_json = self.DATA_DIR / "expected_serialized_commentary.json"
         assert read_json(expected_json) == serialized_json
 
+    def test_serialize(self):
+        serializer = PreAlignedCommentarySerializer()
+        serialized_json = serializer.serialize(
+            self.root_display_pecha, self.root_pecha, self.commentary_pecha
+        )
+
+        expected_json = self.DATA_DIR / "expected_serialized.json"
+        assert read_json(expected_json) == serialized_json
+
     def tearDown(self):
-        pass
-
-
-work = TestPreAlignedCommentarySerializer()
-work.setUp()
-work.test_get_serialized_commentary()
-work.tearDown()
+        self.patcher.stop()
