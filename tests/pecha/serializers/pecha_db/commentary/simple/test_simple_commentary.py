e0dc5155512569defceddbba43fb6093d94239f1
tenzin3 <tenzintsunduebhattu@gmail.com>
2025-03-31 11:18:45 +0000

    fix TestSimpleCommentarySerializer test args

--- tests/pecha/serializers/pecha_db/commentary/simple/test_simple_commentary.py
+++ tests/pecha/serializers/pecha_db/commentary/simple/test_simple_commentary.py
@@ -5,7 +5,7 @@ from openpecha.pecha import Pecha
 from openpecha.pecha.serializers.pecha_db.commentary.simple_commentary import (
     SimpleCommentarySerializer,
 )
-from openpecha.utils import read_json
+from openpecha.utils import read_json, write_json
 
 DATA_DIR = Path(__file__).parent / "data"
 
@@ -32,27 +32,29 @@ class TestSimpleCommentarySerializer(TestCase):
             self.pecha_category,
             "Entering the Middle Way Chapter 6, verses 1 to 64",
         )
+        write_json("bo_commentary_serialized.json", serialized_json)
         expected_serialized_json = read_json(DATA_DIR / "bo/commentary_serialized.json")
         assert serialized_json == expected_serialized_json
 
     def test_en_commentary_serializer(self):
-        pecha = Pecha.from_path(DATA_DIR / "en/I94DBDA91")
-        root_pecha = Pecha.from_path(DATA_DIR / "bo/I6944984E")
+        pecha = Pecha.from_path(DATA_DIR / "bo/I6944984E")
+        translation_pecha = Pecha.from_path(DATA_DIR / "en/I94DBDA91")
 
         serializer = SimpleCommentarySerializer()
         serialized_json = serializer.serialize(
             pecha,
             self.pecha_category,
             "Entering the Middle Way Chapter 6, verses 1 to 64",
-            root_pecha,
+            translation_pecha,
         )
+        write_json("en_commentary_serialized.json", serialized_json)
 
         expected_serialized_json = read_json(DATA_DIR / "en/commentary_serialized.json")
         assert serialized_json == expected_serialized_json
 
     def test_zh_commentary_serializer(self):
-        pecha = Pecha.from_path(DATA_DIR / "zh/I9A60B88D")
-        root_pecha = Pecha.from_path(DATA_DIR / "bo/I6944984E")
+        pecha = Pecha.from_path(DATA_DIR / "bo/I6944984E")
+        translation_pecha = Pecha.from_path(DATA_DIR / "zh/I9A60B88D")
 
         serializer = SimpleCommentarySerializer()
 
@@ -60,7 +62,14 @@ class TestSimpleCommentarySerializer(TestCase):
             pecha,
             self.pecha_category,
             "Entering the Middle Way Chapter 6, verses 1 to 64",
-            root_pecha,
+            translation_pecha,
         )
+        write_json("zh_commentary_serialized.json", serialized_json)
         expected_serialized_json = read_json(DATA_DIR / "zh/commentary_serialized.json")
         assert serialized_json == expected_serialized_json
+
+
+if __name__ == "__main__":
+    import unittest
+
+    unittest.main()
