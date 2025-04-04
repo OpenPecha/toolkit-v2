820cfe3a1b4166f37a7ec9a71b46eb6286f1e7e0
tenzin3 <tenzintsunduebhattu@gmail.com>
2025-04-04 06:14:38 +0000

    decides if root related pecha or commentary related pecha

--- src/openpecha/pecha/parsers/docx/annotation.py
+++ src/openpecha/pecha/parsers/docx/annotation.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from typing import Dict, List, Tuple
 
 from openpecha.pecha import Pecha
+from openpecha.pecha.pecha_types import PechaType, get_pecha_type
 
 pecha_id = str
 layer_name = str
@@ -10,6 +11,27 @@ layer_name = str
 class DocxAnnotationParser:
     def __init__(self):
         pass
+
+    def is_root_related_pecha(self, pecha_type: PechaType) -> bool:
+        """
+        Returns True if the pecha type is root-related.
+        """
+        return pecha_type in [
+            PechaType.root_pecha,
+            PechaType.root_translation_pecha,
+            PechaType.prealigned_root_translation_pecha,
+        ]
+
+    def is_commentary_related_pecha(self, pecha_type: PechaType) -> bool:
+        """
+        Returns True if the pecha type is commentary-related.
+        """
+        return pecha_type in [
+            PechaType.commentary_pecha,
+            PechaType.commentary_translation_pecha,
+            PechaType.prealigned_commentary_pecha,
+            PechaType.prealigned_commentary_translation_pecha,
+        ]
 
     def add_annotation(
         self,
@@ -21,4 +43,12 @@ class DocxAnnotationParser:
         metadatas: List[Dict],
         relation_ship_map: Tuple[pecha_id, layer_name] | None = None,
     ):
-        pass
+        pecha_type: PechaType = get_pecha_type(metadatas)
+
+        if self.is_root_related_pecha(pecha_type):
+            pass
+        elif self.is_commentary_related_pecha(pecha_type):
+            pass
+
+        else:
+            raise ValueError(f"Unknown pecha type: {pecha_type}")
