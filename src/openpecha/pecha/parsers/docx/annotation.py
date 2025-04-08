36eafa1a83fadb687cac39378fc2db739a0f8dff
tenzin3 <tenzintsunduebhattu@gmail.com>
2025-04-08 06:07:16 +0000

    add extract cooridantes in Root

--- src/openpecha/pecha/parsers/docx/annotation.py
+++ src/openpecha/pecha/parsers/docx/annotation.py
@@ -3,6 +3,7 @@ from typing import Dict, List
 
 from openpecha.pecha import Pecha
 from openpecha.pecha.layer import LayerEnum
+from openpecha.pecha.parsers.docx.root.number_list_root import DocxRootParser
 from openpecha.pecha.pecha_types import PechaType, get_pecha_type
 
 pecha_id = str
@@ -46,6 +47,10 @@ class DocxAnnotationParser:
         pecha_type: PechaType = get_pecha_type(metadatas)
 
         if self.is_root_related_pecha(pecha_type):
+            parser = DocxRootParser()
+            segmentation_coords = parser.extract_segmentation_coordinates(  # noqa
+                docx_file
+            )
             pass
         elif self.is_commentary_related_pecha(pecha_type):
             pass
