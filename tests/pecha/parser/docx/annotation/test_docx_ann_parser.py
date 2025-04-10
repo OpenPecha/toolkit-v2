1228c47e16c71b363af22f55812035b6bbc9c1c0
tenzin3 <tenzintsunduebhattu@gmail.com>
2025-04-10 09:47:23 +0000

    revert changes back in TestDocxAnnotationParser

--- tests/pecha/parser/docx/annotation/test_docx_ann_parser.py
+++ tests/pecha/parser/docx/annotation/test_docx_ann_parser.py
@@ -13,14 +13,20 @@ from openpecha.utils import read_json
 class TestDocxAnnotationParser(TestCase):
     def setUp(self):
         self.parser = DocxAnnotationParser()
-        self.root_display_pecha = Pecha.from_path(
-            Path("tests/alignment/commentary_transfer/data/P1/IA6E66F92")
+        self.root_display_pecha_path = Path(
+            "tests/alignment/commentary_transfer/data/P1/IA6E66F92"
         )
+        self.root_display_pecha = Pecha.from_path(self.root_display_pecha_path)
         self.root_display_pecha_metadata = {
             "translation_of": None,
             "commentary_of": None,
             "version_of": None,
             **self.root_display_pecha.metadata.to_dict(),
+        }
+        self.root_display_pecha_backup = {
+            f: f.read_bytes()
+            for f in self.root_display_pecha_path.glob("**/*")
+            if f.is_file()
         }
 
     def test_is_root_related_pecha(self):
@@ -83,7 +89,12 @@ class TestDocxAnnotationParser(TestCase):
         assert new_anns == expected_new_anns
         assert new_annotation_metadata == expected_new_annotation_metadata
 
+    def tearDown(self) -> None:
+        # Revert all original files
+        for f, content in self.root_display_pecha_backup.items():
+            f.write_bytes(content)
 
-work = TestDocxAnnotationParser()
-work.setUp()
-work.test_root_pecha()
+        # Remove any new files that weren't in the original backup
+        for f in self.root_display_pecha_path.glob("**/*"):
+            if f.is_file() and f not in self.root_display_pecha_backup:
+                f.unlink()
