09fac3ab36ffc3c070b02231548ae4f2df4a1092
ta4tsering <ta3tsering@gmail.com>
2025-04-10 11:25:53 +0530

    updated pecha category formatter

--- src/openpecha/pecha/serializers/pecha_db/utils.py
+++ src/openpecha/pecha/serializers/pecha_db/utils.py
@@ -7,30 +7,90 @@ from openpecha.utils import get_text_direction_with_lang
 
 logger = get_logger(__name__)
 
+class FormatPechaCategory:
 
-def format_pecha_category_from_backend(
-    category: List[Dict[str, Dict[str, str]]]
-) -> Dict[str, List[Dict[str, str]]]:
-    """
-    Format the category recieved from backend to our required format.
-    """
-    formatted_category: Dict[str, List[Dict[str, str]]] = {"en": [], "bo": []}
-    for item in category:
-        en_category = {
-            "name": item["name"]["en"],
-            "enDesc": item["description"]["en"],
-            "enShortDesc": item["short_description"]["en"],
+    def __init__(self):
+        self.bo_category = []
+        self.en_category = []
+        self.pecha = None
+        self.bo_root_category = {
+            "name": "རྩ་བ།",
+            "heDesc": "",
+            "heShortDesc": "",
         }
-        bo_category = {
-            "name": item["name"]["bo"],
-            "heDesc": item["description"]["bo"],
-            "heShortDesc": item["short_description"]["bo"],
+        self.en_root_category = {
+            "name": "Root text",
+            "enDesc": "",
+            "enShortDesc": "",
+        }
+        self.bo_commentary_category = {
+            "name": "འགྲེལ་བ།",
+            "heDesc": "",
+            "heShortDesc": "",
+        }
+        self.en_commentary_category = {
+            "name": "Commentary text",
+            "enDesc": "",
+            "enShortDesc": "",
         }
 
-        formatted_category["en"].append(en_category)
-        formatted_category["bo"].append(bo_category)
-    return formatted_category
 
+    def assign_category(self, type: str):
+        if type == "root":
+            self.bo_category.append(self.bo_root_category)
+            self.en_category.append(self.en_root_category)
+        else:
+            self.bo_category.append(self.bo_commentary_category)
+            self.en_category.append(self.en_commentary_category)
+
+
+    def format_root_category(self, pecha: Pecha, pecha_category: Dict[str, List[Dict[str, str]]]):
+        """
+        1.Add Root section ie "རྩ་བ།" or "Root text" to category
+        2.Add pecha title to category
+        """
+        self.bo_category = pecha_category["bo"]
+        self.en_category = pecha_category["en"]
+        self.pecha = pecha
+
+        bo_title = get_pecha_title(self.pecha, "bo")
+        en_title = get_pecha_title(self.pecha, "en")
+        
+        self.assign_category("root")
+
+        self.bo_category.append({"name": bo_title, "heDesc": "", "heShortDesc": ""})
+        self.en_category.append({"name": en_title, "enDesc": "", "enShortDesc": ""})
+
+        return {"bo": self.bo_category, "en": self.en_category}
+
+
+    def format_commentary_category(self, pecha: Pecha, pecha_category: Dict[str, List[Dict[str, str]]], root_title: str):
+        """
+        1.Add Commentary section ie "འགྲེལ་བ།" or "Commentary text" to category
+        2.Add pecha title to category
+        """
+        self.bo_category = pecha_category["bo"]
+        self.en_category = pecha_category["en"]
+        self.pecha = pecha
+        
+        bo_title = get_pecha_title(self.pecha, "bo")
+        en_title = get_pecha_title(self.pecha, "en")
+        
+        self.assign_category("commentary")
+
+        self.bo_category.append({"name": bo_title, "heDesc": "", "heShortDesc": ""})
+        self.en_category.append({"name": en_title, "enDesc": "", "enShortDesc": ""})
+
+        mapping = {
+            "base_text_titles": [root_title],
+            "base_text_mapping": "many_to_one",
+            "link": "Commentary",
+        }
+
+        self.bo_category[-1].update(mapping)
+        self.en_category[-1].update(mapping)
+
+        return {"bo": self.bo_category, "en": self.en_category}
 
 def get_metadata_for_pecha_org(pecha: Pecha, lang: Union[str, None] = None):
     """
