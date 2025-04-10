f970e12ea3af1d7b1b0d66711de19be4d643bd64
gangagyatso4364 <gyatsokhan@gmail.com>
2025-04-10 15:15:56 +0530

    refactor  bdrc utils

--- src/openpecha/bdrc_utils.py
+++ src/openpecha/bdrc_utils.py
@@ -1,10 +1,10 @@
 from pathlib import Path
-from typing import Dict
+from typing import Any, Dict
 
 from openpecha.utils import read_json
 
 
-def extract_metadata_for_work(work_path: Path) -> Dict:
+def extract_metadata_for_work(work_path: Path) -> Dict[str, Any]:
     metadata = {}
     ocr_import_info = read_json(work_path / "ocr_import_info.json")
     metadata["ocr_import_info"] = ocr_import_info
@@ -14,38 +14,24 @@ def extract_metadata_for_work(work_path: Path) -> Dict:
     return metadata
 
 
-def format_metadata_for_op_api(metadata: Dict) -> Dict:
-    NA = "NA"
-    buda_data = metadata["buda_data"]["source_metadata"]
-
-    if buda_data.get("author"):
-        author = buda_data["author"]
-    else:
-        author = NA
-
-    if buda_data.get("title"):
-        title = buda_data["title"]
-    else:
-        title = NA
-
-    if buda_data.get("long_title"):
-        long_title = buda_data["long_title"]
-    else:
-        long_title = NA
-
-    if buda_data.get("languages"):
-        language = buda_data["languages"][0]
-    else:
-        language = NA
+def format_metadata_for_op_api(metadata: Dict[str, Any]) -> Dict[str, Any]:
+    buda_data = metadata.get("buda_data", {}).get("source_metadata", {})
+    document_id = metadata.get("ocr_import_info", {}).get("bdrc_scan_id")
+    author = buda_data.get("author", None)
+    title = buda_data.get("title", None)
+    long_title = buda_data.get("long_title", None)
+    language = (
+        buda_data.get("languages", [None])[0] if buda_data.get("languages") else None
+    )
 
     formatted_data = {
         "bdrc": metadata,
         "author": {"bo": author},
-        "document_id": NA,
+        "document_id": document_id,
         "language": language,
         "long_title": {"bo": long_title},
-        "source_url": buda_data["id"],
-        "title": {"bo": title, "en": NA},
+        "source_url": buda_data.get("id", None),
+        "title": {"bo": title, "en": None},
     }
 
     return formatted_data
