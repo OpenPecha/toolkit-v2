38101f2ef6be68a2e799f5a773d95d915c00d1b5
tenzin3 <tenzintsunduebhattu@gmail.com>
2025-04-10 06:58:33 +0000

    add metdata comparison test docx ann parser

--- tests/pecha/parser/docx/annotation/test_docx_ann_parser.py
+++ tests/pecha/parser/docx/annotation/test_docx_ann_parser.py
@@ -7,6 +7,7 @@ from openpecha.pecha import Pecha, get_anns
 from openpecha.pecha.layer import LayerEnum
 from openpecha.pecha.parsers.docx.annotation import DocxAnnotationParser
 from openpecha.pecha.pecha_types import PechaType
+from openpecha.utils import read_json
 
 
 class TestDocxAnnotationParser(TestCase):
@@ -62,66 +63,27 @@ class TestDocxAnnotationParser(TestCase):
 
         new_anns = get_anns(AnnotationStore(file=str(layer_path)))
         layer_path.unlink()
-        expected_new_anns = [
-            {
-                "root_idx_mapping": "1",
-                "Translation_Segment": "English_Segment",
-                "text": "བུ་མ་འཇུག་པ་ལས་སེམས་བསྐྱེད་དྲུག་པ། ཤོ་ལོ་ཀ ༡-༦༤ མངོན་དུ་ཕྱོགས་པར་མཉམ་བཞག་སེམས་གནས་ཏ",
-            },
-            {
-                "root_idx_mapping": "2",
-                "Translation_Segment": "English_Segment",
-                "text": "། །རྫོགས་པའི་སངས་རྒྱས་ཆོས་ལ་མངོན་ཕྱོགས་",
-            },
-            {
-                "root_idx_mapping": "3",
-                "Translation_Segment": "English_Segment",
-                "text": "ིང༌། །འདི་བརྟེན་འབྱུང་བའི་དེ་ཉིད་མཐ",
-            },
-            {
-                "root_idx_mapping": "4",
-                "Translation_Segment": "English_Segment",
-                "text": "ང་བ་དེས། །ཤེས་རབ་གནས་པས་འགོག་པ་",
-            },
-            {
-                "root_idx_mapping": "5",
-                "Translation_Segment": "English_Segment",
-                "text": "ོབ་པར་འགྱུར། །\nཇི་ལྟར་ལོང་བའི་ཚོགས་ཀུན་བདེ་བླག་ཏུ། །མིག་ལྡན་སྐྱེས་བུ་གཅིག་གིས་འདོད་པ་ཡི། །ཡུལ་དུ་འཁྲིད་པ་དེ་བཞིན་འདིར་ཡང་བློས། །མིག་ཉམས་ཡོན་ཏན་བླངས་",
-            },
-            {
-                "root_idx_mapping": "6",
-                "Translation_Segment": "English_Segment",
-                "text": "ེ་རྒྱལ་ཉིད་འགྲོ། །\nཇི་ལྟར་དེ་ཡིས་ཆེས་ཟབ་ཆོས་རྟོགས་པ། །ལུང་དང་གཞན་ཡང་རིགས་པས་ཡིན་པས་ན། །དེ་ལྟར་འཕགས་པ་ཀླུ་སྒྲུབ་གཞུང་ལུགས་ལས། །ཇི་ལྟར་གནས་པའི་ལ",
-            },
-            {
-                "root_idx_mapping": "7",
-                "Translation_Segment": "English_Segment",
-                "text": "གས་བཞིན་བརྗོད་པར་བྱ། །\nསོ་སོ་སྐྱེ་བོའི་དུས་ནའང་སྟོང་པ་ཉིད་ཐོས་ནས། །ནང་དུ་རབ་ཏུ་དགའ་བ་ཡང་དང་ཡང་དུ་འབྱུང༌། །རབ་ཏུ་དགའ་བ་ལས་བྱུང་མཆི་མས་མིག་བརླན་ཞིང༌། །ལུས་ཀྱི་བ་སྤུ",
-            },
-            {
-                "root_idx_mapping": "8",
-                "Translation_Segment": "English_Segment",
-                "text": "ལྡང་པར་འགྱུར་པ་གང་ཡིན་པ། །\nདེ་ལ་རྫོགས་པའི་སངས་རྒྱས་བློ་ཡི་ས་བོན་ཡོད། །དེ་ཉིད་ཉེ་བར་བསྟན་པའི་སྣོད་ནི་དེ་ཡིན་ཏེ། །དེ་ལ་དམ་པའི་དོན་གྱི་བདེན་པ་བསྟན་པར་བྱ། །དེ་ལ་དེ་ཡི་རྗ",
-            },
-            {
-                "root_idx_mapping": "9",
-                "Translation_Segment": "English_Segment",
-                "text": "ས་སུ་འགྲོ་བའི་ཡོན་ཏན་འབྱུང༌། །\nརྟག་ཏུ་ཚུལ་ཁྲིམས་ཡང་དག་བླངས་ནས་གནས་པར་འགྱུར། །སྦྱིན་པ་གཏོང་བར་འགྱུར་ཞིང་སྙིང་རྗེ་བསྟེན་པར་བྱེད། །བཟོད་པ་སྒོམ་བྱེད་དེ་ཡི་དགེ་བའང་བྱང་ཆུབ་ཏུ། །འགྲོ་བ་དགྲོལ་བ",
-            },
-            {
-                "root_idx_mapping": "10",
-                "Translation_Segment": "English_Segment",
-                "text": "་བྱ་ཕྱིར་ཡོངས་སུ་བསྔོ་བྱེད་ཅིང༌། །\nརྫོགས་པའི་བྱང་ཆུབ་སེམས་དཔའ་རྣམས་ལ་གུས་པར་བྱེད། །ཟབ་ཅིང་རྒྱ་ཆེའི་ཚུལ་ལ་མཁས་པའི་སྐྱེ་བོས་ནི། །རིམ་གྱིས་རབ་ཏུ་དགའ་བའི་ས་ནི་འཐོབ་འགྱུར་བས། །དེ",
-            },
-            {
-                "root_idx_mapping": "11",
-                "Translation_Segment": "English_Segment",
-                "text": "ནི་དོན་དུ་གཉེར་བས་ལམ་འདི་མཉན་པར་གྱིས། །\nདེ་ཉིད་དེ་ལས་འབྱུང་མིན་གཞན་དག་ལས་ལྟ་ག་ལ་ཞིག །གཉིས་ཀ་ལས་ཀྱང་མ་ཡིན་རྒྱུ་མེད་པར་ནི་ག་ལ་ཡོད། །དེ་ནི་དེ་ལས་འབྱུང་ན་ཡོན་ཏན་འགའ་ཡང་ཡོད་མ་ཡིན། །སྐྱེས་པ",
-            },
-            {
-                "root_idx_mapping": "12",
-                "Translation_Segment": "English_Segment",
-                "text": "་གྱུར་པ་སླར་ཡང་སྐྱེ་བར་རིགས་པའང་མ་ཡིན་ཉིད། །\nསྐྱེས་ཟིན་སླར་ཡང་སྐྱེ་བར་ཡོངས་སུ་རྟོག་པར་འགྱུར་ན་ནི། །མྱུ་གུ་ལ་སོགས་རྣམས་ཀྱི་སྐྱེ་བ་འདིར་རྙེད་མི་འགྱུར་ཞིང༌། །ས་བོན་སྲིད་མཐར་ཐུག་པར་རབ་ཏུ་སྐྱེ་བ་ཉིད་དུ་འགྱུར། །ཇི་",
-            },
-        ]
+        expected_new_anns = read_json(
+            Path(
+                "tests/pecha/parser/docx/annotation/data/root_display_pecha/expected_new_anns.json"
+            )
+        )
+
+        metadata = self.root_display_pecha.metadata
+        basename = list(metadata.bases.keys())[0]
+        new_annotation_metadata = metadata.bases[basename]["source_metadata"][
+            "annotations"
+        ][layer_path.stem]
+
+        expected_new_annotation_metadata = {
+            "annotation_title": "དགོངས་པ་རབ་གསལ་ལས་སེམས་བསྐྱེད་དྲུག་པ། ཤོ་ལོ་ཀ ༡-༦༤ segmentation 1",
+            "annotation_type": "Root_Segment",
+        }
+
         assert new_anns == expected_new_anns
+        assert new_annotation_metadata == expected_new_annotation_metadata
+
+
+work = TestDocxAnnotationParser()
+work.setUp()
+work.test_root_pecha()
