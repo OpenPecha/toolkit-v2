adf61067f27778376604c5758ed8b2cd89696ba8
tenzin3 <tenzintsunduebhattu@gmail.com>
2025-04-23 11:21:20 +0000

    Update test paths and remove root display pecha references

--- tests/pecha/parser/docx/annotation/test_docx_ann_parser.py
+++ tests/pecha/parser/docx/annotation/test_docx_ann_parser.py
@@ -12,30 +12,20 @@ from openpecha.utils import read_json
 class TestDocxAnnotationParser(TestCase):
     def setUp(self):
         self.parser = DocxAnnotationParser()
-        self.root_display_pecha_path = Path(
-            "tests/alignment/commentary_transfer/data/P1/IA6E66F92"
-        )
         self.root_pecha_path = Path(
-            "tests/alignment/commentary_transfer/data/P2/IC7760088"
+            "tests/alignment/commentary_transfer/data/root/IA6E66F92"
         )
         self.commentary_pecha_path = Path(
-            "tests/alignment/commentary_transfer/data/P3/I77BD6EA9"
+            "tests/alignment/commentary_transfer/data/commentary/I77BD6EA9"
         )
 
-        self.root_display_pecha = Pecha.from_path(self.root_display_pecha_path)
         self.root_pecha = Pecha.from_path(self.root_pecha_path)
         self.commentary_pecha = Pecha.from_path(self.commentary_pecha_path)
 
-        self.root_display_pecha_metadata = {
-            "translation_of": None,
-            "commentary_of": None,
-            "version_of": None,
-            **self.root_display_pecha.metadata.to_dict(),
-        }
         self.root_pecha_metadata = {
             "translation_of": None,
             "commentary_of": None,
-            "version_of": self.root_display_pecha.id,
+            "version_of": None,
             **self.root_pecha.metadata.to_dict(),
         }
         self.commentary_pecha_metadata = {
@@ -45,10 +35,8 @@ class TestDocxAnnotationParser(TestCase):
             **self.commentary_pecha.metadata.to_dict(),
         }
 
-        self.root_display_pecha_backup = {
-            f: f.read_bytes()
-            for f in self.root_display_pecha_path.glob("**/*")
-            if f.is_file()
+        self.root_pecha_backup = {
+            f: f.read_bytes() for f in self.root_pecha_path.glob("**/*") if f.is_file()
         }
         self.commentary_pecha_backup = {
             f: f.read_bytes()
@@ -61,10 +49,10 @@ class TestDocxAnnotationParser(TestCase):
         docx_file = Path(
             "tests/pecha/parser/docx/annotation/data/root_display_pecha/དགོངས་པ་རབ་གསལ་ལས་སེམས་བསྐྱེད་དྲུག་པ། ཤོ་ལོ་ཀ ༡-༦༤ segmentation 1.docx"
         )
-        metadatas = [self.root_display_pecha_metadata]
+        metadatas = [self.root_pecha_metadata]
 
         pecha, layer_name = self.parser.add_annotation(
-            self.root_display_pecha, type, docx_file, metadatas
+            self.root_pecha, type, docx_file, metadatas
         )
         layer_path = pecha.layer_path / layer_name
         new_anns = get_anns(AnnotationStore(file=str(layer_path)))
@@ -84,13 +72,8 @@ class TestDocxAnnotationParser(TestCase):
         metadatas = [
             self.commentary_pecha_metadata,
             self.root_pecha_metadata,
-            self.root_display_pecha_metadata,
         ]
 
-        parent_layer_path = next(self.root_pecha.layer_path.rglob("*.json"))
-        parent_layer_path = str(
-            parent_layer_path.relative_to(self.root_pecha.pecha_path.parent)
-        )
         pecha, layer_name = self.parser.add_annotation(
             self.commentary_pecha,
             type,
@@ -110,12 +93,12 @@ class TestDocxAnnotationParser(TestCase):
 
     def tearDown(self) -> None:
         # Revert all original files
-        for f, content in self.root_display_pecha_backup.items():
+        for f, content in self.root_pecha_backup.items():
             f.write_bytes(content)
 
         # Remove any new files that weren't in the original backup
-        for f in self.root_display_pecha_path.glob("**/*"):
-            if f.is_file() and f not in self.root_display_pecha_backup:
+        for f in self.root_pecha_path.glob("**/*"):
+            if f.is_file() and f not in self.root_pecha_backup:
                 f.unlink()
 
         # Revert all original files
