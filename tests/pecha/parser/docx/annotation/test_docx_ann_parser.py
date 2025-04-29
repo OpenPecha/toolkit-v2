3f9930b38e392f71622c71049190db3e0f7b2367
tenzin3 <tenzintsunduebhattu@gmail.com>
2025-04-29 03:27:59 +0000

    Refactor docx annotation parser test using SharedPechaSetup class

--- tests/pecha/parser/docx/annotation/test_docx_ann_parser.py
+++ tests/pecha/parser/docx/annotation/test_docx_ann_parser.py
@@ -3,69 +3,17 @@ from unittest import TestCase
 
 from stam import AnnotationStore
 
-from openpecha.pecha import Pecha, get_anns
-from openpecha.pecha.annotations import AnnotationModel, PechaAlignment
+from openpecha.pecha import get_anns
 from openpecha.pecha.layer import LayerEnum
 from openpecha.pecha.parsers.docx.annotation import DocxAnnotationParser
 from openpecha.utils import read_json
+from tests.pecha import SharedPechaSetup
 
 
-class TestDocxAnnotationParser(TestCase):
+class TestDocxAnnotationParser(TestCase, SharedPechaSetup):
     def setUp(self):
+        self.setup_pechas()
         self.parser = DocxAnnotationParser()
-        self.root_pecha_path = Path(
-            "tests/alignment/commentary_transfer/data/root/IA6E66F92"
-        )
-        self.commentary_pecha_path = Path(
-            "tests/alignment/commentary_transfer/data/commentary/I77BD6EA9"
-        )
-
-        self.root_pecha = Pecha.from_path(self.root_pecha_path)
-        self.commentary_pecha = Pecha.from_path(self.commentary_pecha_path)
-
-        self.root_pecha_metadata = {
-            "translation_of": None,
-            "commentary_of": None,
-            "version_of": None,
-            **self.root_pecha.metadata.to_dict(),
-            "annotations": [
-                AnnotationModel(
-                    pecha_id="IA6E66F92",
-                    type=LayerEnum.segmentation,
-                    document_id="d2",
-                    id="B8B3/Segmentation-74F4.json",
-                    title="དགོངས་པ་རབ་གསལ་ལས་སེམས་བསྐྱེད་དྲུག་པ། ཤོ་ལོ་ཀ ༡-༦༤ segmentation",
-                    aligned_to=None,
-                ),
-                AnnotationModel(
-                    pecha_id="IA6E66F92",
-                    type=LayerEnum.segmentation,
-                    document_id="d2",
-                    id="B8B3/Alignment-F81A.json",
-                    title="དགོངས་པ་རབ་གསལ་ལས་སེམས་བསྐྱེད་དྲུག་པ། ཤོ་ལོ་ཀ ༡-༦༤ alignment",
-                    aligned_to=None,
-                ),
-            ],
-        }
-        self.commentary_pecha_metadata = {
-            "translation_of": None,
-            "commentary_of": self.root_pecha.id,
-            "version_of": None,
-            **self.commentary_pecha.metadata.to_dict(),
-            "annotations": [
-                AnnotationModel(
-                    pecha_id="I6944984E",
-                    type=LayerEnum.alignment,
-                    document_id="d4",
-                    id="E949/Alignment-2F29.json",
-                    title="དགོངས་པ་རབ་གསལ་ལས་སེམས་བསྐྱེད་དྲུག་པ། ཤོ་ལོ་ཀ ༡-༦༤ commentary",
-                    aligned_to=PechaAlignment(
-                        pecha_id="IA6E66F92", alignment_id="B8B3/Segmentation-74F4.json"
-                    ),
-                )
-            ],
-        }
-
         self.root_pecha_backup = {
             f: f.read_bytes() for f in self.root_pecha_path.glob("**/*") if f.is_file()
         }
