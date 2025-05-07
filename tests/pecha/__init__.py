4ea320b8ffe11a7bbb0521b57fcb7a25bbd6c302
tenzin3 <tenzintsunduebhattu@gmail.com>
2025-05-07 07:08:23 +0000

    refactor: separate metadata and annotations in pecha test fixtures

--- tests/pecha/__init__.py
+++ tests/pecha/__init__.py
@@ -31,180 +31,182 @@ class SharedPechaSetup:
             "translation_of": None,
             "commentary_of": None,
             **self.root_pecha.metadata.to_dict(),
-            "annotations": [
-                AnnotationModel(
-                    pecha_id="IA6E66F92",
-                    type=AnnotationType.SEGMENTATION,
-                    document_id="d2",
-                    path="B8B3/segmentation-74F4.json",
-                    title="\u0f51\u0f44\u0f0b\u0f42\u0fb1\u0f72\u0f44\u0f0b\u0f54\u0f0b\u0f62\u0f66\u0f0b\u0f63\u0f0b\u0f66\u0f7c\u0f44\u0f0b\u0f62\u0f92\u0fb1\u0f72\u0f0b\u0f0d\u0f66\u0f7c\u0f44\u0f0b\u0f62\u0f92\u0fb1\u0f72\u0f0b\u0f0d segmentation",
-                    aligned_to=None,
-                ),
-                AnnotationModel(
-                    pecha_id="IA6E66F92",
-                    type=AnnotationType.ALIGNMENT,
-                    document_id="d2",
-                    path="B8B3/alignment-74F4.json",
-                    title="\u0f51\u0f44\u0f0b\u0f42\u0fb1\u0f72\u0f44\u0f0b\u0f54\u0f0b\u0f62\u0f66\u0f0b\u0f63\u0f0b\u0f66\u0f7c\u0f44\u0f0b\u0f62\u0f92\u0fb1\u0f72\u0f0b\u0f0d\u0f66\u0f7c\u0f44\u0f0b\u0f62\u0f92\u0fb1\u0f72\u0f0b\u0f0d alignment",
-                    aligned_to=None,
-                ),
-            ],
         }
+        self.root_pecha_annotations = [
+            AnnotationModel(
+                pecha_id="IA6E66F92",
+                type=AnnotationType.SEGMENTATION,
+                document_id="d2",
+                path="B8B3/segmentation-74F4.json",
+                title="\u0f51\u0f44\u0f0b\u0f42\u0fb1\u0f72\u0f44\u0f0b\u0f54\u0f0b\u0f62\u0f66\u0f0b\u0f63\u0f0b\u0f66\u0f7c\u0f44\u0f0b\u0f62\u0f92\u0fb1\u0f72\u0f0b\u0f0d\u0f66\u0f7c\u0f44\u0f0b\u0f62\u0f92\u0fb1\u0f72\u0f0b\u0f0d segmentation",
+                aligned_to=None,
+            ),
+            AnnotationModel(
+                pecha_id="IA6E66F92",
+                type=AnnotationType.ALIGNMENT,
+                document_id="d2",
+                path="B8B3/alignment-74F4.json",
+                title="\u0f51\u0f44\u0f0b\u0f42\u0fb1\u0f72\u0f44\u0f0b\u0f54\u0f0b\u0f62\u0f66\u0f0b\u0f63\u0f0b\u0f66\u0f7c\u0f44\u0f0b\u0f62\u0f92\u0fb1\u0f72\u0f0b\u0f0d\u0f66\u0f7c\u0f44\u0f0b\u0f62\u0f92\u0fb1\u0f72\u0f0b\u0f0d alignment",
+                aligned_to=None,
+            ),
+        ]
 
         self.root_translation_pecha_metadata = {
             "translation_of": "IE60BBDE8",
             "commentary_of": None,
             **self.root_translation_pecha.metadata.to_dict(),
-            "annotations": [
-                AnnotationModel(
-                    pecha_id="I62E00D78",
-                    type=AnnotationType.ALIGNMENT,
-                    document_id="d3",
-                    path="D93E/alignment-0216.json",
-                    title="དགོངས་པ་རབ་གསལ་ལས་སེམས་བསྐྱེད་དྲུག་པ། ཤོ་ལོ་ཀ ༡-༦༤ translation 1",
-                    aligned_to=PechaAlignment(
-                        pecha_id="IE60BBDE8", alignment_id="B8B3/segmentation-74F4.json"
-                    ),
-                )
-            ],
         }
+        self.root_translation_pecha_annotations = [
+            AnnotationModel(
+                pecha_id="I62E00D78",
+                type=AnnotationType.ALIGNMENT,
+                document_id="d3",
+                path="D93E/alignment-0216.json",
+                title="དགོངས་པ་རབ་གསལ་ལས་སེམས་བསྐྱེད་དྲུག་པ། ཤོ་ལོ་ཀ ༡-༦༤ translation 1",
+                aligned_to=PechaAlignment(
+                    pecha_id="IE60BBDE8", alignment_id="B8B3/segmentation-74F4.json"
+                ),
+            )
+        ]
 
         self.commentary_pecha_metadata = {
             "translation_of": None,
             "commentary_of": "IE60BBDE8",
             **self.commentary_pecha.metadata.to_dict(),
-            "annotations": [
-                AnnotationModel(
-                    pecha_id="I6944984E",
-                    type=AnnotationType.ALIGNMENT,
-                    document_id="d4",
-                    path="BEC3/alignment-90C0.json",
-                    title="དགོངས་པ་རབ་གསལ་ལས་སེམས་བསྐྱེད་དྲུག་པ། ཤོ་ལོ་ཀ ༡-༦༤ commentary",
-                    aligned_to=PechaAlignment(
-                        pecha_id="IE60BBDE8", alignment_id="B8B3/segmentation-74F4.json"
-                    ),
-                )
-            ],
         }
+        self.commentary_pecha_annotations = [
+            AnnotationModel(
+                pecha_id="I6944984E",
+                type=AnnotationType.ALIGNMENT,
+                document_id="d4",
+                path="BEC3/alignment-90C0.json",
+                title="དགོངས་པ་རབ་གསལ་ལས་སེམས་བསྐྱེད་དྲུག་པ། ཤོ་ལོ་ཀ ༡-༦༤ commentary",
+                aligned_to=PechaAlignment(
+                    pecha_id="IE60BBDE8", alignment_id="B8B3/segmentation-74F4.json"
+                ),
+            )
+        ]
 
         self.commentary_translation_pecha_metadata = {
             "translation_of": "I6944984E",
             "commentary_of": None,
             **self.commentary_translation_pecha.metadata.to_dict(),
-            "annotations": [
-                AnnotationModel(
-                    pecha_id="I94DBDA91",
-                    type=AnnotationType.ALIGNMENT,
-                    document_id="d4",
-                    path="FD22/alignment-599A.json",
-                    title="\u0f51\u0f44\u0f0b\u0f42\u0fb1\u0f72\u0f44\u0f0b\u0f54\u0f0b\u0f62\u0f66\u0f0b\u0f63\u0f0b\u0f66\u0f7c\u0f44\u0f0b\u0f62\u0f92\u0fb1\u0f72\u0f0b\u0f0d\u0f66\u0f7c\u0f44\u0f0b\u0f62\u0f92\u0fb1\u0f72\u0f0b\u0f0d commentary",
-                    aligned_to=PechaAlignment(
-                        pecha_id="I6944984E", alignment_id="BEC3/alignment-90C0.json"
-                    ),
-                )
-            ],
         }
+        self.commentary_translation_pecha_annotations = [
+            AnnotationModel(
+                pecha_id="I94DBDA91",
+                type=AnnotationType.ALIGNMENT,
+                document_id="d4",
+                path="FD22/alignment-599A.json",
+                title="\u0f51\u0f44\u0f0b\u0f42\u0fb1\u0f72\u0f44\u0f0b\u0f54\u0f0b\u0f62\u0f66\u0f0b\u0f63\u0f0b\u0f66\u0f7c\u0f44\u0f0b\u0f62\u0f92\u0fb1\u0f72\u0f0b\u0f0d\u0f66\u0f7c\u0f44\u0f0b\u0f62\u0f92\u0fb1\u0f72\u0f0b\u0f0d commentary",
+                aligned_to=PechaAlignment(
+                    pecha_id="I6944984E", alignment_id="BEC3/alignment-90C0.json"
+                ),
+            )
+        ]
 
         self.prealigned_root_translation_pecha_metadata = {
             "translation_of": "IE60BBDE8",
             "commentary_of": None,
             **self.root_translation_pecha.metadata.to_dict(),
-            "annotations": [
-                AnnotationModel(
-                    pecha_id="I62E00D78",
-                    type=AnnotationType.ALIGNMENT,
-                    document_id="d3",
-                    path="D93E/alignment-0216.json",
-                    title="དགོངས་པ་རབ་གསལ་ལས་སེམས་བསྐྱེད་དྲུག་པ། ཤོ་ལོ་ཀ ༡-༦༤ translation 1",
-                    aligned_to=PechaAlignment(
-                        pecha_id="IE60BBDE8", alignment_id="B8B3/alignment-F81A.json"
-                    ),
-                )
-            ],
         }
+        self.prealigned_root_translation_pecha_annotations = [
+            AnnotationModel(
+                pecha_id="I62E00D78",
+                type=AnnotationType.ALIGNMENT,
+                document_id="d3",
+                path="D93E/alignment-0216.json",
+                title="དགོངས་པ་རབ་གསལ་ལས་སེམས་བསྐྱེད་དྲུག་པ། ཤོ་ལོ་ཀ ༡-༦༤ translation 1",
+                aligned_to=PechaAlignment(
+                    pecha_id="IE60BBDE8", alignment_id="B8B3/alignment-F81A.json"
+                ),
+            )
+        ]
+
         self.prealigned_root_translation_segmentation_pecha_metadata = {
             "translation_of": "IE60BBDE8",
             "commentary_of": None,
             **self.root_translation_pecha.metadata.to_dict(),
-            "annotations": [
-                AnnotationModel(
-                    pecha_id="I62E00D78",
-                    type=AnnotationType.SEGMENTATION,
-                    document_id="d3",
-                    path="D93E/segmentation-2143.json",
-                    title="དགོངས་པ་རབ་གསལ་ལས་སེམས་བསྐྱེད་དྲུག་པ། ཤོ་ལོ་ཀ ༡-༦༤ segmentation",
-                    aligned_to=PechaAlignment(pecha_id="IE60BBDE8", alignment_id=None),
-                ),
-                AnnotationModel(
-                    pecha_id="I62E00D78",
-                    type=AnnotationType.ALIGNMENT,
-                    document_id="d3",
-                    path="D93E/alignment-0216.json",
-                    title="དགོངས་པ་རབ་གསལ་ལས་སེམས་བསྐྱེད་དྲུག་པ། ཤོ་ལོ་ཀ ༡-༦༤ translation 1",
-                    aligned_to=PechaAlignment(
-                        pecha_id="IE60BBDE8", alignment_id="B8B3/alignment-F81A.json"
-                    ),
-                ),
-            ],
         }
+        self.prealigned_root_translation_segmentation_pecha_annotations = [
+            AnnotationModel(
+                pecha_id="I62E00D78",
+                type=AnnotationType.SEGMENTATION,
+                document_id="d3",
+                path="D93E/segmentation-2143.json",
+                title="དགོངས་པ་རབ་གསལ་ལས་སེམས་བསྐྱེད་དྲུག་པ། ཤོ་ལོ་ཀ ༡-༦༤ segmentation",
+                aligned_to=PechaAlignment(pecha_id="IE60BBDE8", alignment_id=None),
+            ),
+            AnnotationModel(
+                pecha_id="I62E00D78",
+                type=AnnotationType.ALIGNMENT,
+                document_id="d3",
+                path="D93E/alignment-0216.json",
+                title="དགོངས་པ་རབ་གསལ་ལས་སེམས་བསྐྱེད་དྲུག་པ། ཤོ་ལོ་ཀ ༡-༦༤ translation 1",
+                aligned_to=PechaAlignment(
+                    pecha_id="IE60BBDE8", alignment_id="B8B3/alignment-F81A.json"
+                ),
+            ),
+        ]
 
         self.prealigned_commentary_pecha_metadata = {
             "translation_of": None,
             "commentary_of": "IE60BBDE8",
             **self.commentary_pecha.metadata.to_dict(),
-            "annotations": [
-                AnnotationModel(
-                    pecha_id="I6944984E",
-                    type=AnnotationType.ALIGNMENT,
-                    document_id="d4",
-                    path="E949/alignment-2F29.json",
-                    title="དགོངས་པ་རབ་གསལ་ལས་སེམས་བསྐྱེད་དྲུག་པ། ཤོ་ལོ་ཀ ༡-༦༤ commentary",
-                    aligned_to=PechaAlignment(
-                        pecha_id="IE60BBDE8", alignment_id="B8B3/alignment-F81A.json"
-                    ),
-                )
-            ],
         }
+        self.prealigned_commentary_pecha_annotations = [
+            AnnotationModel(
+                pecha_id="I6944984E",
+                type=AnnotationType.ALIGNMENT,
+                document_id="d4",
+                path="E949/alignment-2F29.json",
+                title="དགོངས་པ་རབ་གསལ་ལས་སེམས་བསྐྱེད་དྲུག་པ། ཤོ་ལོ་ཀ ༡-༦༤ commentary",
+                aligned_to=PechaAlignment(
+                    pecha_id="IE60BBDE8", alignment_id="B8B3/alignment-F81A.json"
+                ),
+            )
+        ]
+
         self.prealigned_commentary_segmentation_pecha_metadata = {
             "translation_of": None,
             "commentary_of": "IE60BBDE8",
             **self.commentary_pecha.metadata.to_dict(),
-            "annotations": [
-                AnnotationModel(
-                    pecha_id="I6944984E",
-                    type=AnnotationType.SEGMENTATION,
-                    document_id="d4",
-                    path="E949/segmentation-2134.json",
-                    title="དགོངས་པ་རབ་གསལ་ལས་སེམས་བསྐྱེད་དྲུག་པ། ཤོ་ལོ་ཀ ༡-༦༤ commentary",
-                    aligned_to=PechaAlignment(pecha_id="IE60BBDE8", alignment_id=None),
-                ),
-                AnnotationModel(
-                    pecha_id="I6944984E",
-                    type=AnnotationType.ALIGNMENT,
-                    document_id="d4",
-                    path="E949/alignment-2F29.json",
-                    title="དགོངས་པ་རབ་གསལ་ལས་སེམས་བསྐྱེད་དྲུག་པ། ཤོ་ལོ་ཀ ༡-༦༤ commentary",
-                    aligned_to=PechaAlignment(
-                        pecha_id="IE60BBDE8", alignment_id="B8B3/alignment-F81A.json"
-                    ),
-                ),
-            ],
         }
+        self.prealigned_commentary_segmentation_pecha_annotations = [
+            AnnotationModel(
+                pecha_id="I6944984E",
+                type=AnnotationType.SEGMENTATION,
+                document_id="d4",
+                path="E949/segmentation-2134.json",
+                title="དགོངས་པ་རབ་གསལ་ལས་སེམས་བསྐྱེད་དྲུག་པ། ཤོ་ལོ་ཀ ༡-༦༤ commentary",
+                aligned_to=PechaAlignment(pecha_id="IE60BBDE8", alignment_id=None),
+            ),
+            AnnotationModel(
+                pecha_id="I6944984E",
+                type=AnnotationType.ALIGNMENT,
+                document_id="d4",
+                path="E949/alignment-2F29.json",
+                title="དགོངས་པ་རབ་གསལ་ལས་སེམས་བསྐྱེད་དྲུག་པ། ཤོ་ལོ་ཀ ༡-༦༤ commentary",
+                aligned_to=PechaAlignment(
+                    pecha_id="IE60BBDE8", alignment_id="B8B3/alignment-F81A.json"
+                ),
+            ),
+        ]
 
         self.prealigned_commentary_translation_pecha_metadata = {
             "translation_of": "I6944984E",
             "commentary_of": None,
             **self.commentary_translation_pecha.metadata.to_dict(),
-            "annotations": [
-                AnnotationModel(
-                    pecha_id="I94DBDA91",
-                    type=AnnotationType.ALIGNMENT,
-                    document_id="d4",
-                    path="FD22/alignment-599A.json",
-                    title="དགོངས་པ་རབ་གསལ་ལས་སེམས་བསྐྱེད་དྲུག་པ། ཤོ་ལོ་ཀ ༡-༦༤ commentary translation",
-                    aligned_to=PechaAlignment(
-                        pecha_id="I6944984E", alignment_id="E949/alignment-2F29.json"
-                    ),
-                )
-            ],
         }
+        self.prealigned_commentary_translation_pecha_annotations = [
+            AnnotationModel(
+                pecha_id="I94DBDA91",
+                type=AnnotationType.ALIGNMENT,
+                document_id="d4",
+                path="FD22/alignment-599A.json",
+                title="དགོངས་པ་རབ་གསལ་ལས་སེམས་བསྐྱེད་དྲུག་པ། ཤོ་ལོ་ཀ ༡-༦༤ commentary translation",
+                aligned_to=PechaAlignment(
+                    pecha_id="I6944984E", alignment_id="E949/alignment-2F29.json"
+                ),
+            )
+        ]
