d3439d1ca3d67c298bb3b0f4cd354cfb031d6e7a
tenzin3 <tenzintsunduebhattu@gmail.com>
2025-05-14 16:38:22 +0000

    get metadatachain from metadatatree

--- src/openpecha/pecha/serializers/utils.py
+++ src/openpecha/pecha/serializers/utils.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Optional
+from typing import Any, Dict, List, Optional
 
 from openpecha.pecha.annotations import AnnotationModel
 
@@ -15,3 +15,31 @@ def find_related_pecha_id(
                 return pecha_id
 
     return None
+
+
+def get_metadatachain_from_metadatatree(metadatatree: List[Any], pecha_id: str):
+    """
+    MetadataTree contains metadata of all pecha related to it.
+    Metadatachain is chain from pecha to root pecha.
+    """
+    metadatachain = []
+
+    flag = True
+    while flag:
+        for metadata in metadatatree:
+            if metadata.id == pecha_id:
+                if metadata.commentary_of:
+                    metadatachain.append(metadata)
+                    pecha_id = metadata.commentary_of
+                    break
+
+                if metadata.translation_of:
+                    metadatachain.append(metadata)
+                    pecha_id = metadata.translation_of
+                    break
+
+                flag = False
+                metadatachain.append(metadata)
+                break
+
+    return metadatachain
