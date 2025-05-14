b2d50fdeba256d870df975f02e3cd05a7482706e
tenzin3 <tenzintsunduebhattu@gmail.com>
2025-05-14 17:11:37 +0000

    get metadatachain from metadatatree

--- tests/pecha/serializers/test_serializers_utils.py
+++ tests/pecha/serializers/test_serializers_utils.py
@@ -1,5 +1,10 @@
-from openpecha.pecha.serializers.utils import find_related_pecha_id
-from tests.pecha import SharedPechaSetup
+from unittest import TestCase
+
+from openpecha.pecha.serializers.utils import (
+    find_related_pecha_id,
+    get_metadatachain_from_metadatatree,
+)
+from tests.pecha import DummyMetadataModel, SharedPechaSetup
 
 
 def test_find_related_pecha_id():
@@ -33,3 +38,83 @@ def test_find_related_pecha_id():
     assert (
         find_related_pecha_id(annotations, ann_path) == test_setup.commentary_pecha.id
     )
+
+
+class TestGetMetadataChain(TestCase, SharedPechaSetup):
+    def setUp(self):
+        self.setup_pechas()
+
+        # Set up `id` field in metadatas (Not included in Backend Db)
+        self.root_pecha_metadata = DummyMetadataModel(
+            **{
+                "id": self.root_pecha.id,
+                "translation_of": None,
+                "commentary_of": None,
+                **self.root_pecha.metadata.to_dict(),
+            }
+        )
+        self.root_translation_pecha_metadata = DummyMetadataModel(
+            **{
+                "id": self.root_translation_pecha.id,
+                "translation_of": self.root_pecha.id,
+                "commentary_of": None,
+                **self.root_translation_pecha.metadata.to_dict(),
+            }
+        )
+        self.commentary_pecha_metadata = DummyMetadataModel(
+            **{
+                "id": self.commentary_pecha.id,
+                "translation_of": None,
+                "commentary_of": self.root_pecha.id,
+                **self.commentary_pecha.metadata.to_dict(),
+            }
+        )
+        self.commentary_translation_pecha_metadata = DummyMetadataModel(
+            **{
+                "id": self.commentary_translation_pecha.id,
+                "translation_of": self.commentary_pecha.id,
+                "commentary_of": None,
+                **self.commentary_translation_pecha.metadata.to_dict(),
+            }
+        )
+
+        self.metadatatree = [
+            self.root_pecha_metadata,
+            self.commentary_pecha_metadata,
+            self.commentary_translation_pecha_metadata,
+            self.root_translation_pecha_metadata,
+        ]
+
+    def test_root_pecha(self):
+        metadatachain = get_metadatachain_from_metadatatree(
+            self.metadatatree, self.root_pecha.id
+        )
+        assert metadatachain == [self.root_pecha_metadata]
+
+    def test_root_translation(self):
+        metadatachain = get_metadatachain_from_metadatatree(
+            self.metadatatree, self.root_translation_pecha_metadata.id  # type: ignore
+        )
+        assert metadatachain == [
+            self.root_translation_pecha_metadata,
+            self.root_pecha_metadata,
+        ]
+
+    def test_commentary_pecha(self):
+        metadatachain = get_metadatachain_from_metadatatree(
+            self.metadatatree, self.commentary_pecha.id
+        )
+        assert metadatachain == [
+            self.commentary_pecha_metadata,
+            self.root_pecha_metadata,
+        ]
+
+    def test_commentary_translation_pecha(self):
+        metadatachain = get_metadatachain_from_metadatatree(
+            self.metadatatree, self.commentary_translation_pecha.id
+        )
+        assert metadatachain == [
+            self.commentary_translation_pecha_metadata,
+            self.commentary_pecha_metadata,
+            self.root_pecha_metadata,
+        ]
