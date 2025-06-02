7edd74224a0f447baf429c01c5d258fb696f3e98
tenzin3 <tenzintsunduebhattu@gmail.com>
2025-06-02 11:37:21 +0000

    get foot note content

--- src/openpecha/pecha/parsers/docx/footnote.py
+++ src/openpecha/pecha/parsers/docx/footnote.py
@@ -1,6 +1,6 @@
 import re
 from pathlib import Path
-from typing import List
+from typing import Dict, List
 
 from openpecha.pecha import Pecha
 from openpecha.pecha.annotations import FootnoteAnnotation
@@ -9,11 +9,24 @@ from openpecha.pecha.parsers.docx.utils import read_docx
 
 class DocxFootnoteParser:
     def __init__(self):
-        self.footnote_number_pattern = r"----footnote(\d+)----"
-        self.footnote_content = r"footnote\d+\)[\t\s]+(.+)"
+        self.footnote_number = r"----footnote(\d+)----"
+        self.footnote_content = r"footnote(\d+)\)[\t\s]+(.+)"
+
+    def get_footnote_contents(self, text: str) -> Dict[int, str]:
+        """
+        Extract and remove footnote contents from text.
+        """
+        matches = re.findall(self.footnote_content, text)
+
+        for match in matches:
+            footnote_number = int(match[0])
+            footnote_content = match[1]
+            text = re.sub(self.footnote_content, "", text)
+            footnote_contents[footnote_number] = footnote_content
+        return footnote_contents
 
     def extract_footnote(self, text: str) -> List[FootnoteAnnotation]:
-        matches = re.findall(self.footnote_pattern, text)
+        matches = re.findall(self.footnote_number, text)
         for match in matches:
             # get footnote number
             footnote_number = match.group(1)
