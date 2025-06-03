8266295491d874de8dadf8a2781375f4d0b6090e
tenzin3 <tenzintsunduebhattu@gmail.com>
2025-06-03 09:31:30 +0000

    fix test update coords in TestFootnoteParser

--- tests/pecha/parser/docx/footnote/test_footnote_parser.py
+++ tests/pecha/parser/docx/footnote/test_footnote_parser.py
@@ -47,12 +47,12 @@ class TestFootnoteParser(TestCase):
                 note="功德光論師，世親菩薩弟子，極善巧毗奈耶，出生於秣搜羅國婆羅門家，幼時 熟習外道宗義及明處，後於家鄉出家並受具足戒，依止世親菩薩，修習大小乘 藏，及聲聞十八部派一切宗義，弟子約五千比丘。住世說約四百年，是否屬實， 猶存疑惑，然而確是住世很久。後圓寂於自己的故鄉。著作頗豐，譯為藏文廣 知者，有《菩薩地所分佈施次第第九之上注釋》、《菩薩地戒品釋》，此二者 由那措譯師及香智軍二位譯為藏文，存於論典經品「ཡི」函中。《毗奈耶根本經》 及其自釋、《毗奈耶行持一百零一竭摩法》，在八世紀末，赤松德贊時，由覺 柔譯師及噶榮戒源二位譯為藏文，存於論典經品「ཟུ」、「འུ」、「ཡུ」。",
             ),
             FootnoteAnnotation(
-                span=Span(start=13, end=13, errors=None),
+                span=Span(start=24, end=24, errors=None),
                 metadata=None,
                 note="《瑜伽師地論》中《本地分》菩薩地之菩薩戒品。",
             ),
             FootnoteAnnotation(
-                span=Span(start=26, end=26, errors=None),
+                span=Span(start=53, end=53, errors=None),
                 metadata=None,
                 note="《菩薩地戒品》說:「云何菩薩自性戒?謂若略說具四功德，當知是名菩薩自性戒。何等為四?一、從他正受。二、善淨意樂。三、犯已還淨。四、深敬專念無有違犯。」",
             ),
@@ -96,7 +96,7 @@ class TestFootnoteParser(TestCase):
         new_base = self.pecha.get_base(list(self.pecha.bases.keys())[0])
         updated_anns = update_coords(
             self.expected_annotations,
-            self.expected_text_without_footnote_content,
+            self.expected_text_without_footnote_spans,
             new_base,
         )
         self.assertEqual(updated_anns, self.expected_updated_anns)
