16f6d7ee50d829e8bb753d58591d5dcc088e3eb4
tenzin3 <tenzintsunduebhattu@gmail.com>
2025-06-03 07:12:28 +0000

    compare annotations for test create footnote layer

--- tests/pecha/parser/docx/footnote/test_footnote_parser.py
+++ tests/pecha/parser/docx/footnote/test_footnote_parser.py
@@ -1,7 +1,9 @@
 from pathlib import Path
 from unittest import TestCase, mock
 
-from openpecha.pecha import Pecha
+from stam import AnnotationStore
+
+from openpecha.pecha import Pecha, get_anns
 from openpecha.pecha.annotations import FootnoteAnnotation, Span
 from openpecha.pecha.layer import AnnotationType
 from openpecha.pecha.parsers.docx.footnote import DocxFootnoteParser
@@ -106,6 +108,34 @@ class TestFootnoteParser(TestCase):
             self.pecha, self.expected_updated_anns, AnnotationType.FOOTNOTE
         )
         self.assertEqual(annotation_path, "B4A6/footnote-D93E.json")
+
+        anns = get_anns(
+            ann_store=AnnotationStore(
+                file=str(self.pecha.layer_path / annotation_path)
+            ),
+            include_span=True,
+        )
+        expected_anns = [
+            {
+                "note": "功德光論師，世親菩薩弟子，極善巧毗奈耶，出生於秣搜羅國婆羅門家，幼時 熟習外道宗義及明處，後於家鄉出家並受具足戒，依止世親菩薩，修習大小乘 藏，及聲聞十八部派一切宗義，弟子約五千比丘。住世說約四百年，是否屬實， 猶存疑惑，然而確是住世很久。後圓寂於自己的故鄉。著作頗豐，譯為藏文廣 知者，有《菩薩地所分佈施次第第九之上注釋》、《菩薩地戒品釋》，此二者 由那措譯師及香智軍二位譯為藏文，存於論典經品「ཡི」函中。《毗奈耶根本經》 及其自釋、《毗奈耶行持一百零一竭摩法》，在八世紀末，赤松德贊時，由覺 柔譯師及噶榮戒源二位譯為藏文，存於論典經品「ཟུ」、「འུ」、「ཡུ」。",
+                "structure_type": "footnote",
+                "text": "",
+                "Span": {"start": 13, "end": 13},
+            },
+            {
+                "note": "《瑜伽師地論》中《本地分》菩薩地之菩薩戒品。",
+                "structure_type": "footnote",
+                "text": "",
+                "Span": {"start": 13, "end": 13},
+            },
+            {
+                "note": "《菩薩地戒品》說:「云何菩薩自性戒?謂若略說具四功德，當知是名菩薩自性戒。何等為四?一、從他正受。二、善淨意樂。三、犯已還淨。四、深敬專念無有違犯。」",
+                "structure_type": "footnote",
+                "text": "",
+                "Span": {"start": 26, "end": 26},
+            },
+        ]
+        self.assertEqual(anns, expected_anns)
 
     def tearDown(self):
         # Revert all original files
