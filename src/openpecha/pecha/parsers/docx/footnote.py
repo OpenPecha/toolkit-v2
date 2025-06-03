df5c990dfd8c98cc2c68c417e60a6170a3aa933a
tenzin3 <tenzintsunduebhattu@gmail.com>
2025-06-03 05:34:09 +0000

    add footnote layer func

--- src/openpecha/pecha/parsers/docx/footnote.py
+++ src/openpecha/pecha/parsers/docx/footnote.py
@@ -1,8 +1,10 @@
 import re
 from pathlib import Path
-from typing import Dict, Tuple
+from typing import Dict, List, Tuple
 
 from openpecha.pecha import Pecha
+from openpecha.pecha.annotations import BaseAnnotation, FootnoteAnnotation, Span
+from openpecha.pecha.layer import AnnotationType
 from openpecha.pecha.parsers.docx.utils import read_docx
 
 
@@ -47,6 +49,33 @@ class DocxFootnoteParser:
         text = re.sub(self.footnote_number, "", text)
         return (text, footnote_spans)
 
-    def parse(self, pecha: Pecha, input: str | Path):
+    def add_footnote_layer(
+        self, pecha: Pecha, anns: List[BaseAnnotation], ann_type: AnnotationType
+    ):
+
+        basename = list(pecha.bases.keys())[0]
+        layer, layer_path = pecha.add_layer(basename, ann_type)
+        for ann in anns:
+            pecha.add_annotation(layer, ann, ann_type)
+        layer.save()
+
+        return str(layer_path.relative_to(pecha.layer_path))
+
+    def parse(self, pecha: Pecha, input: str | Path) -> str:
         text = read_docx(input)
-        text, footnote_contents = self.get_footnote_contents(text)  # noqa
+        text, footnote_contents = self.get_footnote_contents(text)
+        text, footnote_spans = self.get_footnote_spans(text, footnote_contents)
+
+        anns = []
+        for footnote_number, span in footnote_spans.items():
+            anns.append(
+                FootnoteAnnotation(
+                    span=Span(start=span[0], end=span[1]),
+                    note=footnote_contents[footnote_number],
+                )
+            )
+
+        annotation_path: str = self.add_footnote_layer(
+            pecha, anns, AnnotationType.FOOTNOTE
+        )
+        return annotation_path
