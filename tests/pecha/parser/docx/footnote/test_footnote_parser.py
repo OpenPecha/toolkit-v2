169330c3822d445ae899e8416a46ce7a5514225a
tenzin3 <tenzintsunduebhattu@gmail.com>
2025-06-03 06:29:57 +0000

    test for create footnote layer

--- tests/pecha/parser/docx/footnote/test_footnote_parser.py
+++ tests/pecha/parser/docx/footnote/test_footnote_parser.py
@@ -1,8 +1,9 @@
 from pathlib import Path
-from unittest import TestCase
+from unittest import TestCase, mock
 
 from openpecha.pecha import Pecha
 from openpecha.pecha.annotations import FootnoteAnnotation, Span
+from openpecha.pecha.layer import AnnotationType
 from openpecha.pecha.parsers.docx.footnote import DocxFootnoteParser
 from openpecha.pecha.parsers.docx.utils import read_docx, update_coords
 
@@ -37,9 +38,30 @@ class TestFootnoteParser(TestCase):
             ),
         ]
 
+        self.expected_updated_anns = [
+            FootnoteAnnotation(
+                span=Span(start=13, end=13, errors=None),
+                metadata=None,
+                note="功德光論師，世親菩薩弟子，極善巧毗奈耶，出生於秣搜羅國婆羅門家，幼時 熟習外道宗義及明處，後於家鄉出家並受具足戒，依止世親菩薩，修習大小乘 藏，及聲聞十八部派一切宗義，弟子約五千比丘。住世說約四百年，是否屬實， 猶存疑惑，然而確是住世很久。後圓寂於自己的故鄉。著作頗豐，譯為藏文廣 知者，有《菩薩地所分佈施次第第九之上注釋》、《菩薩地戒品釋》，此二者 由那措譯師及香智軍二位譯為藏文，存於論典經品「ཡི」函中。《毗奈耶根本經》 及其自釋、《毗奈耶行持一百零一竭摩法》，在八世紀末，赤松德贊時，由覺 柔譯師及噶榮戒源二位譯為藏文，存於論典經品「ཟུ」、「འུ」、「ཡུ」。",
+            ),
+            FootnoteAnnotation(
+                span=Span(start=13, end=13, errors=None),
+                metadata=None,
+                note="《瑜伽師地論》中《本地分》菩薩地之菩薩戒品。",
+            ),
+            FootnoteAnnotation(
+                span=Span(start=26, end=26, errors=None),
+                metadata=None,
+                note="《菩薩地戒品》說:「云何菩薩自性戒?謂若略說具四功德，當知是名菩薩自性戒。何等為四?一、從他正受。二、善淨意樂。三、犯已還淨。四、深敬專念無有違犯。」",
+            ),
+        ]
+
         self.pecha = Pecha.from_path(
             Path("tests/pecha/parser/docx/footnote/data/I926CCA43")
         )
+        self.pecha_backup = {
+            f: f.read_bytes() for f in self.pecha.pecha_path.glob("**/*") if f.is_file()
+        }
 
     def test_get_footnote_contents(self):
         text = read_docx(self.one_page_footnote, ignore_footnotes=False)
@@ -75,23 +97,22 @@ class TestFootnoteParser(TestCase):
             self.expected_text_without_footnote_content,
             new_base,
         )
+        self.assertEqual(updated_anns, self.expected_updated_anns)
 
-        expected_updated_anns = [
-            FootnoteAnnotation(
-                span=Span(start=13, end=13, errors=None),
-                metadata=None,
-                note="功德光論師，世親菩薩弟子，極善巧毗奈耶，出生於秣搜羅國婆羅門家，幼時 熟習外道宗義及明處，後於家鄉出家並受具足戒，依止世親菩薩，修習大小乘 藏，及聲聞十八部派一切宗義，弟子約五千比丘。住世說約四百年，是否屬實， 猶存疑惑，然而確是住世很久。後圓寂於自己的故鄉。著作頗豐，譯為藏文廣 知者，有《菩薩地所分佈施次第第九之上注釋》、《菩薩地戒品釋》，此二者 由那措譯師及香智軍二位譯為藏文，存於論典經品「ཡི」函中。《毗奈耶根本經》 及其自釋、《毗奈耶行持一百零一竭摩法》，在八世紀末，赤松德贊時，由覺 柔譯師及噶榮戒源二位譯為藏文，存於論典經品「ཟུ」、「འུ」、「ཡུ」。",
-            ),
-            FootnoteAnnotation(
-                span=Span(start=13, end=13, errors=None),
-                metadata=None,
-                note="《瑜伽師地論》中《本地分》菩薩地之菩薩戒品。",
-            ),
-            FootnoteAnnotation(
-                span=Span(start=26, end=26, errors=None),
-                metadata=None,
-                note="《菩薩地戒品》說:「云何菩薩自性戒?謂若略說具四功德，當知是名菩薩自性戒。何等為四?一、從他正受。二、善淨意樂。三、犯已還淨。四、深敬專念無有違犯。」",
-            ),
-        ]
+    @mock.patch("openpecha.pecha.get_layer_id")
+    def test_create_footnote_layer(self, mock_get_layer_id):
+        mock_get_layer_id.return_value = "D93E"
+        annotation_path = self.parser.add_footnote_layer(
+            self.pecha, self.expected_updated_anns, AnnotationType.FOOTNOTE
+        )
+        self.assertEqual(annotation_path, "B4A6/footnote-D93E.json")
 
-        self.assertEqual(updated_anns, expected_updated_anns)
+    def tearDown(self):
+        # Revert all original files
+        for f, content in self.pecha_backup.items():
+            f.write_bytes(content)
+
+        # Remove any new files that weren't in the original backup
+        for f in self.pecha.pecha_path.glob("**/*"):
+            if f.is_file() and f not in self.pecha_backup:
+                f.unlink()
