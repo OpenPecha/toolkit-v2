4401b5006345ec1f8e30faf619dde9c05b849828
tenzin3 <tenzintsunduebhattu@gmail.com>
2025-07-11 05:59:41 +0000

    add alignment layer test to TestJsonSerializer get_annotations

--- tests/pecha/serializers/json/test_json_serializer.py
+++ tests/pecha/serializers/json/test_json_serializer.py
@@ -7,14 +7,18 @@ from openpecha.pecha.serializers.json_serializer import JsonSerializer
 
 class TestJsonSerializer(TestCase):
     def setUp(self):
-        self.pecha_path = Path(
+        self.root_pecha_path = Path(
             "tests/alignment/commentary_transfer/data/root/I6556B464"
         )
-        self.pecha = Pecha.from_path(self.pecha_path)
+        self.commentary_pecha_path = Path(
+            "tests/alignment/commentary_transfer/data/commentary/I015AFFA7"
+        )
+        self.root_pecha = Pecha.from_path(self.root_pecha_path)
+        self.commentary_pecha = Pecha.from_path(self.commentary_pecha_path)
 
     def test_get_base(self):
         serializer = JsonSerializer()
-        base = serializer.get_base(self.pecha)
+        base = serializer.get_base(self.root_pecha)
 
         expected_base = Path(
             "tests/alignment/commentary_transfer/data/root/I6556B464/base/B5FE.txt"
@@ -23,10 +27,11 @@ class TestJsonSerializer(TestCase):
 
     def test_get_annotations(self):
         serializer = JsonSerializer()
-        layer_name = "B5FE/segmentation-4FD1.json"
 
+        # SEGMENTATION
+        layer_name = "B5FE/segmentation-4FD1.json"
         annotations = serializer.get_annotations(
-            pecha=self.pecha, layer_name=layer_name
+            pecha=self.root_pecha, layer_name=layer_name
         )
         expected_annotations = [
             {"id": "59EBB25F49", "index": 1, "segmentation_type": "segmentation"},
@@ -38,6 +43,69 @@ class TestJsonSerializer(TestCase):
             {"id": "8725807438", "index": 7, "segmentation_type": "segmentation"},
             {"id": "D2BACBFB51", "index": 8, "segmentation_type": "segmentation"},
             {"id": "E9DCF0CBEB", "index": 9, "segmentation_type": "segmentation"},
+        ]
+        assert annotations == expected_annotations
+
+        # Alignment Layer
+        layer_name = "B014/alignment-2127.json"
+        annotations = serializer.get_annotations(
+            pecha=self.commentary_pecha, layer_name=layer_name
+        )
+        expected_annotations = [
+            {
+                "id": "5ED5D59969",
+                "index": 1,
+                "alignment_index": "1",
+                "segmentation_type": "alignment",
+            },
+            {
+                "id": "9DAD7F460F",
+                "index": 2,
+                "alignment_index": "2",
+                "segmentation_type": "alignment",
+            },
+            {
+                "id": "6850060CDD",
+                "index": 3,
+                "alignment_index": "3",
+                "segmentation_type": "alignment",
+            },
+            {
+                "id": "88F8B42309",
+                "index": 4,
+                "alignment_index": "4",
+                "segmentation_type": "alignment",
+            },
+            {
+                "id": "1F86B74B46",
+                "index": 5,
+                "alignment_index": "5",
+                "segmentation_type": "alignment",
+            },
+            {
+                "id": "E92B454ED1",
+                "index": 6,
+                "alignment_index": "6",
+                "segmentation_type": "alignment",
+            },
+            {
+                "id": "63937BDD48",
+                "index": 7,
+                "alignment_index": "7",
+                "segmentation_type": "alignment",
+            },
+            {
+                "id": "BC7A7FDA98",
+                "index": 8,
+                "alignment_index": "8",
+                "segmentation_type": "alignment",
+            },
+            {
+                "id": "23834A44D3",
+                "index": 9,
+                "alignment_index": "9",
+                "segmentation_type": "alignment",
+            },
         ]
         assert annotations == expected_annotations
 
