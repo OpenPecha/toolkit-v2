2bf9dd5c2b67d3e84e839c40a8e459ef74f7d775
tenzin3 <tenzintsunduebhattu@gmail.com>
2025-07-30 08:58:43 +0000

    remove unused exceptions

--- src/openpecha/exceptions.py
+++ src/openpecha/exceptions.py
@@ -10,20 +10,8 @@ class EmptyFileError(Exception):
     pass
 
 
-class MetaDataMissingError(Exception):
-    """Raised when the metadata is missing."""
-
-    pass
-
-
 class MetaDataValidationError(Exception):
     """Raised when the metadata is not valid."""
-
-    pass
-
-
-class StamAnnotationStoreLoadError(Exception):
-    """Raised when there is an error loading annotation store in STAM."""
 
     pass
 
@@ -42,15 +30,5 @@ class ParseNotReadyForThisAnnotation(Exception):
 
 class InValidAnnotationLayerName(Exception):
     """Raised when the layer name is not associated with any Annotations"""
-
-    pass
-
-
-class AnnotationLayerIsNotSegmentationOrAlignment(Exception):
-    """Raise when a annotation layer is not Segmentation or Alignment Layer"""
-
-    def __init__(self, pecha_id: str, layer_name: str) -> None:
-        message = f"Pecha {pecha_id} layer {layer_name} is not segmentation or alignment layer to map other layer."
-        super().__init__(message)
 
     pass
